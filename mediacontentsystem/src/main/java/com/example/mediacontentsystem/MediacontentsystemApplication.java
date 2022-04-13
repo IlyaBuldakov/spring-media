c5e32642f4d5ccd0264122ef989acf1f353b2a58
petrovk <petrovk86@mail.ru>
2022-04-14 00:23:33 +0400

    fix(idea): форматирование кода

--- mediacontentsystem/src/main/java/com/example/mediacontentsystem/MediacontentsystemApplication.java
+++ mediacontentsystem/src/main/java/com/example/mediacontentsystem/MediacontentsystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MediacontentsystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MediacontentsystemApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MediacontentsystemApplication.class, args);
+  }
 
 }
