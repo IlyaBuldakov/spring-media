7578f213197c7ee59971cb657ece30b8d9596cda
petrovk <petrovk86@mail.ru>
2022-04-13 21:47:37 +0400

    feat: добавлена заготовка проекта

---
+++ mediacontentsystem/src/main/java/com/example/mediacontentsystem/MediacontentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediacontentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediacontentsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediacontentsystemApplication.class, args);
+    }
+
+}
---
+++ mediacontentsystem/src/test/java/com/example/mediacontentsystem/MediacontentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediacontentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediacontentsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
