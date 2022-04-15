03ba03192e503544232abcbbbd260318173f875e
IlyaBuldakov <ggpanguin@gmail.com>
2022-04-15 12:32:16 +0400

    feat: Создана сущность пользователя

---
+++ src/main/java/domain/entities/user/User.java
@@ -0,0 +1,49 @@
+package domain.entities.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Класс, описывающий пользователя
+ *
+ * @author IlyaBuldakov
+ */
+
+@AllArgsConstructor
+public class User {
+
+    /**
+     * Идентификатор пользователя
+     *
+     * @return id Идентификатор пользователя
+     */
+    private @Getter int id;
+
+    /**
+     * Имя пользователя
+     *
+     * @return name Имя пользователя
+     */
+    private @Getter String name;
+
+    /**
+     * E-mail пользователя
+     *
+     * @return email E-mail пользователя
+     */
+    private @Getter String email;
+
+    /**
+     * Аватар пользователя
+     *
+     * @return avatar Аватар пользователя
+     */
+    private @Getter byte[] avatar;
+
+    /**
+     * Роль пользователя {@link UserRole.RoleType}
+     *
+     * @return role Роль пользователя
+     */
+    private @Getter UserRole role;
+}
