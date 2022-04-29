efeca3d923c7a9c23cb3d546cd78c33ae9c6cf86
IlyaBuldakov <ggpanguin@gmail.com>
2022-04-29 12:23:35 +0400

    feat: Добавлена реализация получения списка всех пользователей

---
+++ src/main/java/domain/usecases/user/GetAllUsers.java
@@ -0,0 +1,25 @@
+package domain.usecases.user;
+
+import domain.entities.failures.Failure;
+import domain.entities.user.User;
+import domain.repositories.UserRepository;
+import domain.usecases.UseCase;
+import io.vavr.control.Either;
+
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ * Реализация сценария получения всех пользователей
+ *
+ * @author IlyaBuldakov
+ */
+public class GetAllUsers implements UseCase<Void, List<User>> {
+
+    UserRepository userRepository;
+
+    @Override
+    public Future<Either<Failure, List<User>>> execute(Void param) {
+        return userRepository.getAll();
+    }
+}
