0bb4fdf702a826dcf28236f6343a00c32faeafe2
petrovk <petrovk86@mail.ru>
2022-05-03 19:33:56 +0400

    feat: добавлен репозиторий для пользователя

---
+++ mediacontentsystem/src/main/java/com/example/mediacontentsystem/domain/repositories/UserRepository.java
@@ -0,0 +1,31 @@
+package com.example.mediacontentsystem.domain.repositories;
+
+import com.example.mediacontentsystem.domain.entities.failures.Failure;
+import com.example.mediacontentsystem.domain.entities.user.Role;
+import com.example.mediacontentsystem.domain.entities.user.User;
+import com.example.mediacontentsystem.domain.repositories.base.CrudRepository;
+import io.vavr.control.Either;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ * Интерфейс репозитория для пользователей.
+ */
+public interface UserRepository extends CrudRepository<User> {
+  /**
+   * Список пользователей по имени.
+   *
+   * @param name имя пользователя.
+   * @return Список пользователей по имени.
+   */
+  Future<Either<Failure, List<User>>> filter(String name);
+
+  /**
+   * Список пользователей по имени и роли.
+   *
+   * @param name имя пользователя.
+   * @param role роль пользователя.
+   * @return Список пользователей по имени и роли.
+   */
+  Future<Either<Failure, List<User>>> filter(String name, Role role);
+}
---
+++ mediacontentsystem/src/main/java/com/example/mediacontentsystem/domain/repositories/base/CrudRepository.java
@@ -0,0 +1,49 @@
+package com.example.mediacontentsystem.domain.repositories.base;
+
+import com.example.mediacontentsystem.domain.entities.failures.Failure;
+import io.vavr.control.Either;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ * CRUD Интерфейс.
+ *
+ * @param <EntityT> Тип сущности.
+ */
+public interface CrudRepository<EntityT> {
+  /**
+   * Создает сущность.
+   *
+   * @param entity сущность.
+   */
+  Future<Either<Failure, EntityT>> create(EntityT entity);
+
+  /**
+   * Получает сущность по идентификатору.
+   *
+   * @param id идентификатор сущности.
+   * @return Получает сущность по идентификатору.
+   */
+  Future<Either<Failure, EntityT>> get(int id);
+
+  /**
+   * Получает список всех сущностей.
+   *
+   * @return Получает список всех сущностей.
+   */
+  Future<Either<Failure, List<EntityT>>> getAll();
+
+  /**
+   * Обновляет сущность.
+   *
+   * @param entity сущность.
+   */
+  Future<Either<Failure, EntityT>> update(EntityT entity);
+
+  /**
+   * Удаляет сущность.
+   *
+   * @param id идентификатор сущности.
+   */
+  Future<Either<Failure, Void>> delete(int id);
+}
