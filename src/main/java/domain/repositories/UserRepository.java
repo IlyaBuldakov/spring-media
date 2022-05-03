ca816a906b673f232c9cca9d87d4c418a938b822
YuriyKrasnoperov <yurakr89@gmail.com>
2022-05-03 18:08:39 +0400

    fix: Доработка репозитория пользователя.

--- src/main/java/domain/repositories/UserRepository.java
+++ src/main/java/domain/repositories/UserRepository.java
@@ -1,6 +1,12 @@
 package domain.repositories;
 
 import domain.entities.User;
+import domain.entities.UserRole;
+import domain.entities.failures.Failure;
+import io.vavr.control.Either;
+import java.util.List;
+import java.util.concurrent.Future;
+
 
 /**
  * Репозиторий пользователя.
@@ -10,8 +16,10 @@ public interface UserRepository {
    * Добавление нового пользователя.
    *
    * @param user - новый пользователь
+   *
+   * @return user - новый пользователь, подробнее {@link User}
    */
-  void add(User user);
+  Future<Either<Failure, User>> add(User user);
 
   /**
    * Получение пользователя.
@@ -20,19 +28,48 @@ public interface UserRepository {
    *
    * @return user - пользователь, подробнее {@link User}
    */
-  User get(int id);
+  Future<Either<Failure, User>> get(int id);
+
+  /**
+   * Получение всех пользователей.
+   *
+   * @return list - список всех пользователей, подробнее {@link User}
+   */
+  Future<Either<Failure, List<User>>> getAll();
 
   /**
    * Обновление пользователя.
    *
    * @param user - пользователь, подробнее {@link User}
+   *
+   * @return user - пользователь, подробнее {@link User}
    */
-  void update(User user);
+  Future<Either<Failure, User>> update(User user);
 
   /**
    * Удаление пользователя.
    *
    * @param id - идентификатор пользователя
    */
-  void delete(int id);
+  Future<Either<Failure, Void>> delete(int id);
+
+  /**
+   * Получение списка пользователей, чье имя соответствует строке запроса {@code query}.
+   *
+   * @param query - строка запроса
+   *
+   * @return list - список пользователей, подробнее {@link User}
+   */
+  Future<Either<Failure, List<User>>> search(String query);
+
+  /**
+   * Получение списка пользователей, чье имя и роль соответствует
+   * строке запроса {@code query} и роли {@code role}.
+   *
+   * @param query - строка запроса
+   * @param role - роль пользователя, подробнее {@link UserRole}
+   *
+   * @return list - список пользователей, подробнее {@link User}
+   */
+  Future<Either<Failure, List<User>>> search(String query, UserRole role);
 }
