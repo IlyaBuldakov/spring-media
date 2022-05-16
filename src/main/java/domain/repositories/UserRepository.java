f12bf1d74e3c25f33f1838fa13c97fdb0d9e1430
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-05-16 22:51:05 +0400

    feat: Добавлен репозиторий пользователей.

---
+++ src/main/java/domain/repositories/UserRepository.java
@@ -0,0 +1,70 @@
+package domain.repositories;
+
+import domain.entities.failures.Failure;
+import domain.entities.user.RoleDto;
+import domain.entities.user.User;
+import io.vavr.control.Either;
+
+import java.util.List;
+import java.util.concurrent.Future;
+
+
+/**
+ * Репозиторий пользователей.
+ */
+public interface UserRepository {
+  /**
+   * Создаёт пользователя.
+   *
+   * @param user Пользователь.
+   */
+  Future<Either<Failure, User>> create(User user);
+
+  /**
+   * Обновляет данные пользователя.
+   *
+   * @param user Пользователь.
+   */
+  Future<Either<Failure, User>> update(User user);
+
+  /**
+   * Удаляет пользоваетеля.
+   *
+   * @param id Идентификатор пользователя.
+   */
+  Future<Either<Failure, Void>> delete(int id);
+
+  /**
+   * Получает пользователя.
+   *
+   * @param id Идентификатор пользователя.
+   * @return Пользователь.
+   */
+  Future<Either<Failure, User>> get(int id);
+
+  /**
+   * Получает список всех пользователей.
+   *
+   * @return Список пользователей.
+   */
+  Future<Either<Failure, List<User>>> getAll();
+
+  /**
+   * Получает список пользователей, чьё имя соответствует
+   * строке запроса {@code query}.
+   *
+   * @param query Строка запроса.
+   * @return Список пользователей.
+   */
+  Future<Either<Failure, List<User>>> search(String query);
+
+  /**
+   * Получает список пользователей, чьё имя и роль соответствуют
+   * строке запроса {@code query} и роли {@code role}.
+   *
+   * @param query Строка запроса.
+   * @param role  Роль пользователя.
+   * @return Список пользователей.
+   */
+  Future<Either<Failure, List<User>>> search(String query, RoleDto role);
+}
