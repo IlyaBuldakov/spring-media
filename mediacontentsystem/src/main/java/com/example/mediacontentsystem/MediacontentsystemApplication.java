20b9a4e488578b118afb062dee6a8b22dad69cad
petrovk <petrovk86@mail.ru>
2022-05-21 21:34:29 +0400

    fix(checkstyle): удален модификатор public

--- mediacontentsystem/src/main/java/com/example/mediacontentsystem/MediacontentsystemApplication.java
+++ mediacontentsystem/src/main/java/com/example/mediacontentsystem/MediacontentsystemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MediacontentsystemApplication {
+class MediacontentsystemApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MediacontentsystemApplication.class, args);
