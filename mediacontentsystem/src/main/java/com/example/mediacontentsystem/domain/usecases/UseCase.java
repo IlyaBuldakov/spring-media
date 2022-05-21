9e959259d611652a3942053c67cc3d7fe770c845
petrovk <petrovk86@mail.ru>
2022-05-21 21:20:26 +0400

    fix: изменен на вариант не ожидающий параметры

--- mediacontentsystem/src/main/java/com/example/mediacontentsystem/domain/usecases/UseCase.java
+++ mediacontentsystem/src/main/java/com/example/mediacontentsystem/domain/usecases/UseCase.java
@@ -7,9 +7,8 @@ import java.util.concurrent.Future;
 /**
  * Сценарий использования.
  *
- * @param <ParamT> Параметры сценария.
  * @param <ResultT> Результат работы сценария.
  */
-public interface UseCase<ParamT, ResultT> {
-  Future<Either<Failure, ResultT>> execute(ParamT param);
+public interface UseCase<ResultT> {
+  Future<Either<Failure, ResultT>> execute();
 }
