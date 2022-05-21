89fcdc76719174178062910c7c1eb7ce92239855
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-05-21 14:51:17 +0400

    feat: Добавлены тесты для сценария получения пользователя по идентификатору

---
+++ src/test/java/domain/usecases/user/GetUserByIdTest.java
@@ -0,0 +1,78 @@
+package domain.usecases.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import domain.entities.failures.NotFoundDto;
+import domain.entities.user.User;
+import domain.repositories.UserRepository;
+import domain.usecases.UseCase;
+import io.vavr.control.Either;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class GetUserByIdTest {
+  final UserRepository mockUserRepository = mock(UserRepository.class);
+  final GetUserById useCase = new GetUserById(mockUserRepository);
+
+  @Test
+  void shouldInheritUseCase() {
+    // Assert
+    assertThat(useCase).isInstanceOf(UseCase.class);
+  }
+
+  @Test
+  void shouldGetUserFromTheRepository() {
+    // Arrange
+    var userId = new Random().nextInt();
+
+    // Act
+    useCase.execute(userId);
+
+    // Assert
+    verify(mockUserRepository).get(userId);
+  }
+
+  @Test
+  void userExists_ShouldReturnUser() throws ExecutionException, InterruptedException {
+    // Arrange
+    var userId = new Random().nextInt();
+    var user = User.createTestUser(userId);
+
+    when(mockUserRepository.get(userId))
+            .thenReturn(CompletableFuture.completedFuture(Either.right(user)));
+
+    // Act
+    var result = useCase.execute(userId)
+            .get()
+            .get();
+
+    // Assert
+    assertThat(result).isEqualTo(user);
+  }
+
+  @Test
+  void userDoesNotExist_ShouldReturnNotFound() throws ExecutionException, InterruptedException {
+    // Arrange
+    var userId = new Random().nextInt();
+    var failure = new NotFoundDto();
+
+    when(mockUserRepository.get(userId))
+            .thenReturn(CompletableFuture.completedFuture(Either.left(failure)));
+
+    // Act
+    var result = useCase.execute(userId)
+            .get()
+            .getLeft();
+
+    // Assert
+    assertThat(result).isEqualTo(failure);
+  }
+}
