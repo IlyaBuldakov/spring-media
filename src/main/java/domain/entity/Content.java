ecedadb09d49e81985fe62b93710659b0ec9e133
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-05-23 19:42:15 +0400

    fix: в класс Content добавлены поля

--- src/main/java/domain/entity/Content.java
+++ src/main/java/domain/entity/Content.java
@@ -1,4 +1,58 @@
 package domain.entity;
 
+import domain.entity.content.ContentFormat;
+import domain.entity.task.ContentType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
+/**
+ * Контент
+ */
+@AllArgsConstructor
+@NoArgsConstructor
 public class Content {
+    /**
+     * Идентификатор контента
+     */
+    @Getter private Integer id;
+
+    /**
+     * Тип контента
+     */
+    @Getter private ContentType contentType;
+
+    /**
+     * Название контента
+     */
+    @Getter private String name;
+
+    /**
+     * Дата создания
+     */
+    @Getter private LocalDateTime dateCreated;
+
+    /**
+     * Автор контента
+     */
+    @Getter private User author;
+
+    /**
+     * Тип контента
+     */
+    @Getter private ContentFormat format;
+
+    /**
+     * Путь к файлу
+     */
+    @Getter private Path url;
+
+    /**
+     * Путь к превью
+     */
+    @Getter private Path preview;
+
 }
