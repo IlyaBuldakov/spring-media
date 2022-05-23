71bdf774d47cfb500a89e0f7bb67959b58cb1292
Yaroslav <yaroslav-w@mail.ru>
2022-05-23 23:39:22 +0400

    feat: Добавлен сценарий обновления данных пользователя и тесты для него.

---
+++ src/test/java/domain/usecases/user/UpdateUserTest.java
@@ -0,0 +1,99 @@
+package domain.usecases.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import domain.entities.failures.NotFound;
+import domain.entities.user.User;
+import domain.entities.user.UserRole;
+import domain.repositories.UserRepository;
+import domain.usecases.UseCase;
+import io.vavr.control.Either;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateUserTest {
+  final UserRepository mockUserRepository = mock(UserRepository.class);
+  final UpdateUser useCase = new UpdateUser(mockUserRepository);
+
+  @Test
+  void shouldInheritUseCase() {
+    // Assert
+    assertThat(useCase).isInstanceOf(UseCase.class);
+  }
+
+  @Test
+  void shouldUpdateUserByTheRepository() {
+    // Arrange
+    var user = new User(
+        new Random().nextInt(),
+        "user@example.com",
+        "Passw0rd!",
+        "Иванов Иван",
+        new byte[] {},
+        new UserRole(1, UserRole.RoleType.ADMIN)
+    );
+
+    // Act
+    useCase.execute(user);
+
+    // Assert
+    verify(mockUserRepository).update(user);
+  }
+
+  @Test
+  void usersExist_ShouldUpdateUser() throws ExecutionException, InterruptedException {
+    // Arrange
+    var user = new User(
+        new Random().nextInt(),
+        "user@example.com",
+        "Passw0rd!",
+        "Иванов Иван",
+        new byte[] {},
+        new UserRole(1, UserRole.RoleType.ADMIN)
+    );
+
+    when(mockUserRepository.update(user))
+        .thenReturn(CompletableFuture.completedFuture(Either.right(user)));
+
+    // Act
+    var result = useCase.execute(user)
+        .get()
+        .get();
+
+    // Assert
+    assertThat(result).isEqualTo(user);
+  }
+
+  @Test
+  void usersNotExist_ShouldReturnNotFound() throws ExecutionException, InterruptedException {
+    // Arrange
+    var user = new User(
+        new Random().nextInt(),
+        "user@example.com",
+        "Passw0rd!",
+        "Иванов Иван",
+        new byte[] {},
+        new UserRole(1, UserRole.RoleType.ADMIN)
+    );
+    var failure = new NotFound();
+
+    when(mockUserRepository.update(user))
+        .thenReturn(CompletableFuture.completedFuture(Either.left(failure)));
+
+    // Act
+    var result = useCase.execute(user)
+        .get()
+        .getLeft();
+
+    // Assert
+    assertThat(result).isEqualTo(failure);
+  }
+}
