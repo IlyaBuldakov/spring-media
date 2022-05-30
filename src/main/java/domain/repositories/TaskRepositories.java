53372ba328d1813a9643b57da9ad0f072a1aaccc
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-05-30 20:17:35 +0400

    feat: добавлен интерфейс TaskRepositories

---
+++ src/main/java/domain/repositories/TaskRepositories.java
@@ -0,0 +1,49 @@
+package domain.repositories;
+
+import domain.entity.errors.Failure;
+import domain.entity.task.Task;
+import io.vavr.control.Either;
+
+import java.util.concurrent.Future;
+
+/**
+ * Репозиторий задач
+ */
+
+public interface TaskRepositories {
+    /**
+     * Создаёт задачу.
+     *
+     * @param task Задача.
+     */
+    Future<Either<Failure, Task>> create(Task task);
+
+    /**
+     * Обновляет задачу.
+     *
+     * @param task задача.
+     */
+    Future<Either<Failure, Task>> update(Task task);
+
+    /**
+     * Удаляет задачу.
+     *
+     * @param id Идентификатор задачу.
+     */
+    Future<Either<Failure, Void>> delete(int id);
+
+    /**
+     * Получает пользователя.
+     *
+     * @param id Идентификатор задачи.
+     * @return задача.
+     */
+    Future<Either<Failure, Task>> get(int id);
+
+    /**
+     * Получает список всех задач.
+     *
+     * @return Список задач.
+     */
+    Future<Either<Failure, Iterable<Task>>> getAll();
+}
