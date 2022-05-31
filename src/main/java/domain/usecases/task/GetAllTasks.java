d13f121044da079b9b395feaddf0f79534214fe8
igro.kiryanov.1986@mail.ru <dERp@r01>
2022-05-31 16:31:18 +0400

    fix: обновлены классы usecase Task

--- src/main/java/domain/usecases/task/GetAllTasks.java
+++ src/main/java/domain/usecases/task/GetAllTasks.java
@@ -2,17 +2,21 @@ package domain.usecases.task;
 
 import domain.entity.errors.Failure;
 import domain.entity.task.Task;
+import domain.repositories.TaskRepositories;
 import domain.usecases.UseCase;
+import infrastructure.repositories.FakeTaskRepositories;
 import io.vavr.control.Either;
 
 import java.util.concurrent.Future;
 
 /**
- *  Получить список всех задач
+ * Получить список всех задач
  */
 public class GetAllTasks implements UseCase<Void, Iterable<Task>> {
+    TaskRepositories repositories = new FakeTaskRepositories();
+
     @Override
     public Future<Either<Failure, Iterable<Task>>> execute(Void param) {
-        return null;
+        return repositories.getAll();
     }
 }
--- src/main/java/domain/usecases/task/GetTaskById.java
+++ src/main/java/domain/usecases/task/GetTaskById.java
@@ -2,14 +2,20 @@ package domain.usecases.task;
 
 import domain.entity.errors.Failure;
 import domain.entity.task.Task;
+import domain.repositories.TaskRepositories;
 import domain.usecases.UseCase;
+import infrastructure.repositories.FakeTaskRepositories;
 import io.vavr.control.Either;
 
 import java.util.concurrent.Future;
 
+/**
+ * Получить задачу по Id
+ */
 public class GetTaskById implements UseCase<Integer, Task> {
+    TaskRepositories repositories = new FakeTaskRepositories();
     @Override
     public Future<Either<Failure, Task>> execute(Integer id) {
-        return null;
+        return repositories.get(id);
     }
 }
