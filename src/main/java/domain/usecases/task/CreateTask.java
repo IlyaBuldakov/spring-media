fcdaed916e05a8d815031529073a4db2553234e0
igro.kiryanov.1986@mail.ru <dERp@r01>
2022-06-01 10:02:44 +0400

    Revert "fix: обновлены классы usecase Task"
    This reverts commit d13f121044da079b9b395feaddf0f79534214fe8.

--- src/main/java/domain/usecases/task/CreateTask.java
+++ src/main/java/domain/usecases/task/CreateTask.java
@@ -2,17 +2,14 @@ package domain.usecases.task;
 
 import domain.entity.errors.Failure;
 import domain.entity.task.Task;
-import domain.repositories.TaskRepositories;
 import domain.usecases.UseCase;
-import infrastructure.repositories.FakeTaskRepositories;
 import io.vavr.control.Either;
 
 import java.util.concurrent.Future;
 
 public class CreateTask implements UseCase<Task, Task> {
-    TaskRepositories repositories = new FakeTaskRepositories();
     @Override
     public Future<Either<Failure, Task>> execute(Task task) {
-        return repositories.create(task);
+        return null;
     }
 }
--- src/main/java/domain/usecases/task/DeleteTaskById.java
+++ src/main/java/domain/usecases/task/DeleteTaskById.java
@@ -1,17 +1,14 @@
 package domain.usecases.task;
 
 import domain.entity.errors.Failure;
-import domain.repositories.TaskRepositories;
 import domain.usecases.UseCase;
-import infrastructure.repositories.FakeTaskRepositories;
 import io.vavr.control.Either;
 
 import java.util.concurrent.Future;
 
 public class DeleteTaskById implements UseCase<Integer, Void> {
-    TaskRepositories repositories = new FakeTaskRepositories();
     @Override
-    public Future<Either<Failure, Void>> execute(Integer id) {
-        return repositories.delete(id);
+    public Future<Either<Failure, Void>> execute(Integer param) {
+        return null;
     }
 }
