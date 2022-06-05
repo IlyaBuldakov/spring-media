975c7e6aee00ac306c832a3718deaf89f9fb9027
YuriyKrasnoperov <yurakr89@gmail.com>
2022-06-05 16:24:40 +0400

    feat: Добавлены репозиторий и сценарий использования сущности комментария.

---
+++ src/main/java/com/htc/domain/repositories/CommentRepository.java
@@ -0,0 +1,20 @@
+package com.htc.domain.repositories;
+
+import com.htc.domain.entities.comment.Comment;
+import com.htc.domain.entities.failures.Failure;
+import io.vavr.control.Either;
+import java.util.concurrent.Future;
+
+/**
+ * Репозиторий для комментария.
+ */
+public interface CommentRepository {
+  /**
+   * Добавление нового комментария в задачу.
+   *
+   * @param comment - новый комментарий
+   *
+   * @return comment - новый комментарий, подробнее {@link Comment}
+   */
+  Future<Either<Failure, Comment>> add(Comment comment);
+}
---
+++ src/main/java/com/htc/domain/usecases/comment/AddComment.java
@@ -0,0 +1,24 @@
+package com.htc.domain.usecases.comment;
+
+import com.htc.domain.entities.comment.Comment;
+import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.repositories.CommentRepository;
+import com.htc.domain.usecases.UseCase;
+import io.vavr.control.Either;
+import java.util.concurrent.Future;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Сценарий добавления нового комментария в задачу.
+ */
+@Component
+@AllArgsConstructor
+public final class AddComment implements UseCase<Comment, Comment> {
+  private final CommentRepository repository;
+
+  @Override
+  public Future<Either<Failure, Comment>> execute(Comment comment) {
+    return repository.add(comment);
+  }
+}
