5f4b23a0f1ac9075f7457c1f684222153f9b98ac
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-06-06 23:05:40 +0400

    feat: Добавлен сценарий обновления файла и тесты для него.

---
+++ src/test/java/domain/usecases/file/UpdateFileTest.java
@@ -0,0 +1,76 @@
+package domain.usecases.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import domain.entities.failures.NotFound;
+import domain.entities.files.File;
+import domain.repositories.FileRepository;
+import domain.usecases.UseCase;
+import io.vavr.control.Either;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateFileTest {
+  final FileRepository mockFileRepository = mock(FileRepository.class);
+  final UpdateFile useCase = new UpdateFile(mockFileRepository);
+
+  @Test
+  void shouldInheritUseCase() {
+    // Assert
+    assertThat(useCase).isInstanceOf(UseCase.class);
+  }
+
+  @Test
+  void shouldUpdateFileByTheRepository() {
+    // Arrange
+    var file = File.createTestFile();
+
+    // Act
+    useCase.execute(file);
+
+    // Assert
+    verify(mockFileRepository).update(file);
+  }
+
+  @Test
+  void fileExist_ShouldUpdateUserAndReturnFile() throws ExecutionException, InterruptedException {
+    // Arrange
+    var file = File.createTestFile();
+
+    when(mockFileRepository.update(file))
+            .thenReturn(CompletableFuture.completedFuture(Either.right(file)));
+
+    // Act
+    var result = useCase.execute(file)
+            .get()
+            .get();
+
+    // Assert
+    assertThat(result).isEqualTo(file);
+  }
+
+  @Test
+  void fileNotExist_ShouldReturnNotFound() throws ExecutionException, InterruptedException {
+    // Arrange
+    var file = File.createTestFile();
+    var failure = new NotFound();
+
+    when(mockFileRepository.update(file))
+            .thenReturn(CompletableFuture.completedFuture(Either.left(failure)));
+
+    // Act
+    var result = useCase.execute(file)
+            .get()
+            .getLeft();
+
+    // Assert
+    assertThat(result).isEqualTo(failure);
+  }
+}
