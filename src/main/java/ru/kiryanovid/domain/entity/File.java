d37acb21bbc212f9dee64d20779f119117c5a665
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-06-07 20:06:54 +0400

    feat:в класс File добавлены поля

--- src/main/java/ru/kiryanovid/domain/entity/File.java
+++ src/main/java/ru/kiryanovid/domain/entity/File.java
@@ -1,4 +1,38 @@
 package ru.kiryanovid.domain.entity;
 
-public class File {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import ru.kiryanovid.domain.entity.file.Format;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ */
+@AllArgsConstructor
+public class File { /**
+ *
+ */
+@Getter
+private Integer id;
+
+    /**
+     *
+     */
+    @Getter private String name;
+
+    /**
+     *
+     */
+    @Getter private LocalDateTime dateCreate;
+
+    /**
+     *
+     */
+    @Getter private Format format;
+
+    /**
+     *
+     */
+    @Getter private String url;
 }
