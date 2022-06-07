1a9f27f8207382c01f1fcbad84ececd581ee611e
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-06-07 19:58:04 +0400

    feat: добавлены поля в класс Comment

--- src/main/java/ru/kiryanovid/domain/entity/Comment.java
+++ src/main/java/ru/kiryanovid/domain/entity/Comment.java
@@ -1,4 +1,29 @@
 package ru.kiryanovid.domain.entity;
 
+import lombok.Getter;
+import ru.kiryanovid.application.dto.users.UserBasicDto;
+
+import java.time.LocalDateTime;
+
 public class Comment {
+    /**
+     *
+     */
+    @Getter
+    private Integer id;
+
+    /**
+     *
+     */
+    @Getter private LocalDateTime date;
+
+    /**
+     *
+     */
+    @Getter private UserBasicDto user;
+
+    /**
+     *
+     */
+    @Getter private String message;
 }
