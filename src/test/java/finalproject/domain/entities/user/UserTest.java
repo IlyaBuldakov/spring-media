22d23fd2019d1fc7f49b636472b30285772b6ba6
Ilya Voenkov <voenkov@gmail.com>
2022-06-10 15:41:51 +0400

    feat: глупые консольные тесты

--- src/test/java/finalproject/domain/entities/user/UserTest.java
+++ src/test/java/finalproject/domain/entities/user/UserTest.java
@@ -1,17 +1,47 @@
 package finalproject.domain.entities.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import finalproject.infrastructure.FakeUserRepository;
 import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class UserTest {
-  User user = new User("ava@mail.ru", "вася", "assdfasfsf", "abcdefghewwert1A", Role.ADMIN);
+  static ObjectMapper mapper = new ObjectMapper();
+  static void toConsole(Object obj) throws JsonProcessingException {
+    System.out.println(mapper.writeValueAsString(obj));
+    return;
+  }
 
   @Test
-  void User_get_email() {
-    assertThat(user.getEmail()).isEqualTo("ava@mail.ru");
+  void Test() throws JsonProcessingException {
+    FakeUserRepository repository = new FakeUserRepository();
+    repository.save(User.createRandomFakeUser());
+    repository.save(User.createRandomFakeUser());
+    repository.save(User.createRandomFakeUser());
+    repository.save(User.createRandomFakeUser());
+    repository.deleteById(2);
+
+    toConsole(repository.findAll());
+    User user1 = repository.findById(1).get();
+    user1.setName("Василий Хренов");
+    toConsole(repository.findAll());;
+    repository.save(user1);
+    toConsole(repository.findAll());
+
+
+
   }
+
+
+    //void User_get_email() {
+    //assertThat(user.getEmail()).isEqualTo("ava@mail.ru");
+
 }
