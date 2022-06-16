f4440320b7b0d1e6bb4612ceddcf55ebe6ba8892
Ilya Voenkov <voenkov@gmail.com>
2022-06-16 17:11:16 +0400

    feat: в класс Приложения добавлена поддержка асинхронных операций

--- src/main/java/finalproject/Main.java
+++ src/main/java/finalproject/Main.java
@@ -2,12 +2,15 @@ package finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Главный класс программы.
  */
 @SpringBootApplication
-public class Main {
+@EnableAsync
+public class Main extends AsyncConfigurerSupport {
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
