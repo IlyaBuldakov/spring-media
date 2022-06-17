244c18c6147275a6617fbe126af5a874ce03869c
IlyaBuldakov <ggpanguin@gmail.com>
2022-06-17 14:36:32 +0400

    feat: Добавлена аннотация @EnableAsync

--- src/main/java/com/htc/Application.java
+++ src/main/java/com/htc/Application.java
@@ -2,12 +2,14 @@ package com.htc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author IlyaBuldakov
  */
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
