6ea60db6484648e81a755f1755da9a7015a83c9e
IlyaBuldakov <ggpanguin@gmail.com>
2022-06-17 12:56:46 +0400

    feat: Добавлен класс для валидации различных данных

---
+++ src/main/java/com/htc/util/ValuesValidator.java
@@ -0,0 +1,18 @@
+package com.htc.util;
+
+import com.htc.domain.entities.failures.Failure;
+/**
+ * Класс для валидации данных
+ *
+ * @author IlyaBuldakov
+ */
+public class ValuesValidator {
+
+    public static Failure checkUserFields(int id,
+                                          String name,
+                                          String password,
+                                          String email,
+                                          byte[] avatar) {
+        return null;
+    }
+}
