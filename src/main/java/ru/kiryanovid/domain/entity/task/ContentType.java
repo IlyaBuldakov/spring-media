a07b1e2f87b4484679d3be2508fc87b81a7e4b92
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-06-20 20:41:28 +0400

    fix: Изменены перечисления ContentType и Status.

--- src/main/java/ru/kiryanovid/domain/entity/task/ContentType.java
+++ src/main/java/ru/kiryanovid/domain/entity/task/ContentType.java
@@ -4,7 +4,15 @@ package ru.kiryanovid.domain.entity.task;
  * Тип контента
  */
 public enum ContentType {
-    VIDEO,
-    AUDIO,
-    PHOTO
+    VIDEO(1, "video"),
+    AUDIO(2, "audio"),
+    PHOTO(3, "photo");
+
+    private int id;
+    private String name;
+
+    ContentType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/ru/kiryanovid/domain/entity/task/Status.java
+++ src/main/java/ru/kiryanovid/domain/entity/task/Status.java
@@ -1,8 +1,18 @@
 package ru.kiryanovid.domain.entity.task;
 
 /**
- *
+ * Статус контента.
  */
 public enum Status {
-    IN_WORK, FEEDBACK, APPROVED
+    IN_WORK(1, "inWork"),
+    FEEDBACK(2, "feedback"),
+    APPROVED(3, "approved");
+
+    private int id;
+    private String name;
+
+    Status(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
