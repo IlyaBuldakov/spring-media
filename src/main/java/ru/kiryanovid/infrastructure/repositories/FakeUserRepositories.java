74f8e3f50cd52f0fe2796f4186d7a320c2701a0d
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-06-22 20:53:35 +0400

    feat: создан фейковый репозиторй для класса User

---
+++ src/main/java/ru/kiryanovid/infrastructure/repositories/FakeUserRepositories.java
@@ -0,0 +1,36 @@
+package ru.kiryanovid.infrastructure.repositories;
+
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Component;
+import ru.kiryanovid.domain.entity.users.Role;
+import ru.kiryanovid.domain.entity.users.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+@Component
+public class FakeUserRepositories {
+    private static final Faker faker = Faker.instance(new Locale("ru"));
+    private static final List<User> userList = new ArrayList<>();
+
+    static {
+        var roles = Role.values();
+        var size = new Random().nextInt(10);
+        while (size-- > 0){
+            var user = User.create(
+                    size,
+                    faker.name().fullName(),
+                    faker.internet().emailAddress(),
+                    faker.internet().password(5, 17) + "1aA",
+                    faker.lorem().characters(40),
+                    roles[new Random().nextInt(roles.length)]
+            ).get();
+            userList.add(user);
+        }
+    }
+    public User randomGet(){
+        return userList.get(new Random().nextInt(userList.size()-1));
+    }
+}
