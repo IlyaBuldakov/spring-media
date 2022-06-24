485e5f2f18f6cbb70a4b904a0b0e593756f3ad89
Ilya Voenkov <voenkov@gmail.com>
2022-06-24 10:53:25 +0400

    fix: поправлен тип возвращаемого значения у методов контроллера

--- src/main/java/finalproject/application/controllers/UserController.java
+++ src/main/java/finalproject/application/controllers/UserController.java
@@ -7,6 +7,8 @@ import finalproject.application.services.UserService;
 import finalproject.domain.entities.failures.Failure;
 import finalproject.domain.entities.user.User;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @AllArgsConstructor
@@ -27,24 +30,27 @@ public class UserController {
   public List<User> getUsers() throws ExecutionException, InterruptedException {
     return userservice.getAllUsers().get().get();
   }
+
+
+
   @GetMapping("/createRandom")
-  public User createRandom() throws ExecutionException, InterruptedException {
+  public CompletableFuture<User> createRandom() {
     return userservice.createNewUser(User.createRandomFakeUser()
             .getOrElseThrow(failure -> new BadRequestDto(failure, (FieldInvalidDto[]) Arrays
                     .stream(failure.getProblems())
                     .map(problem -> new FieldInvalidDto(problem))
-                    .toArray())))
-                    .get()
-            .getOrElseThrow(failure -> new InternalServerErrorDto(failure));
+                    .toArray()))).thenApply(either -> either.getOrElseThrow(failure -> new InternalServerErrorDto(failure)));
+
+
   }
 
   @GetMapping("/{id}")
-  public User getUser(@PathVariable int id) throws ExecutionException, InterruptedException {
-    return userservice.getUserById(id).get().get();}
+  public CompletableFuture<User> getUser(@PathVariable int id) throws ExecutionException, InterruptedException {
+    return userservice.getUserById(id).thenApply(either -> either.get());}
 
   @GetMapping("/deleteUser/{id}")
-  public void deleteUser(@PathVariable int id) throws ExecutionException, InterruptedException {
-    userservice.deleteUserById(id);
+  public CompletableFuture<Void> deleteUser(@PathVariable int id) {
+    return userservice.deleteUserById(id).thenApply(either -> either.getOrElseThrow(failure -> new InternalServerErrorDto(failure)));
   }
 
 
