5c8dd4886b8f422be3b451508dd156d61d4a89c7
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-06-28 22:01:36 +0400

    fix: Изменен принцип формирования списка задач.

--- src/main/java/ru/kiryanovid/infrastructure/repositories/FakeTaskRepositories.java
+++ src/main/java/ru/kiryanovid/infrastructure/repositories/FakeTaskRepositories.java
@@ -3,18 +3,20 @@ package ru.kiryanovid.infrastructure.repositories;
 import com.github.javafaker.Faker;
 import io.vavr.control.Either;
 import org.springframework.stereotype.Component;
-import ru.kiryanovid.domain.entity.content.Content;
 import ru.kiryanovid.domain.entity.errors.Failure;
 import ru.kiryanovid.domain.entity.errors.InvalidValue;
 import ru.kiryanovid.domain.entity.task.ContentType;
 import ru.kiryanovid.domain.entity.task.Status;
 import ru.kiryanovid.domain.entity.task.Task;
+import ru.kiryanovid.domain.entity.users.User;
 import ru.kiryanovid.domain.repositories.TaskRepositories;
 
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
@@ -24,55 +26,33 @@ import java.util.concurrent.Future;
 @Component
 public class FakeTaskRepositories implements TaskRepositories {
     private static final Faker faker = Faker.instance(new Locale("ru"));
-    private static FakeUserRepositories fur = new FakeUserRepositories();
-    private static final List<Task> taskList = List.of(
-            Task.create(
-                    1,
-                    faker.lorem().characters(50),
-                    ContentType.AUDIO,
-                    faker.lorem().characters(50),
-                    Path.of(faker.file().fileName()),
-                    fur.randomGet(),
-                    fur.randomGet(),
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusDays(1),
-                    new Content(),
-                    List.of(),
-                    Status.APPROVED
-            ).get(),
-            Task.create(
-                    2,
-                    faker.lorem().characters(50),
-                    ContentType.AUDIO,
-                    faker.lorem().characters(50),
-                    Path.of(faker.file().fileName()),
-                    fur.randomGet(),
-                    fur.randomGet(),
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusDays(1),
-                    new Content(),
-                    List.of(),
-                    Status.FEEDBACK
-            ).get(),
-            Task.create(
-                    3,
-                    faker.lorem().characters(50),
-                    ContentType.AUDIO,
-                    faker.lorem().characters(50),
-                    Path.of(faker.file().fileName()),
-                    fur.randomGet(),
-                    fur.randomGet(),
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusDays(1),
-                    new Content(),
-                    List.of(),
-                    Status.IN_WORK
-            ).get()
-    );
+    private static List<User> userList = new FakeUserRepositories().getList();
+    private static final List<Task> taskList = new ArrayList<>();
 
-   // public FakeTaskRepositories(FakeUserRepositories fur) {
-   //     this.fur = fur;
-   // }
+    static {
+        var status = Status.values();
+        var contentType = ContentType.values();
+        var repoSize = 5;
+        while (repoSize >= 0){
+            var task = Task.create( new Random().nextInt(255),
+                    faker.lorem().characters(50),
+                    contentType[new Random().nextInt(contentType.length)],
+                    faker.lorem().characters(50),
+                    Path.of(faker.file().fileName()),
+                    userList.get(new Random().nextInt(userList.size())),
+                    userList.get(new Random().nextInt(userList.size())),
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusDays(1),
+                    null,
+                    null,
+                    status[new Random().nextInt(status.length)]
+            ).get();
+
+            var tempTask = task;
+            taskList.add(task);
+            repoSize--;
+        }
+    }
 
     @Override
     public CompletableFuture<Either<Failure, Task>> create(Task task) {
