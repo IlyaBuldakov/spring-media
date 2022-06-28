0147c4a2ef209fe6127dbf3c5658628ec724d270
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-06-28 22:03:18 +0400

    fix: удалены лишнее импорты из TaskController.

--- src/main/java/ru/kiryanovid/application/controllers/TaskController.java
+++ src/main/java/ru/kiryanovid/application/controllers/TaskController.java
@@ -2,10 +2,9 @@ package ru.kiryanovid.application.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ru.kiryanovid.application.dto.errors.InternalServerErrorDto;
+import ru.kiryanovid.application.dto.errors.NotFoundDto;
 import ru.kiryanovid.application.dto.task.TaskDto;
 import ru.kiryanovid.application.dto.task.TaskListDto;
-import ru.kiryanovid.domain.entity.errors.Failure;
 import ru.kiryanovid.domain.entity.task.Task;
 import ru.kiryanovid.domain.usecases.task.*;
 
@@ -31,7 +30,7 @@ public class TaskController {
                 .get();
         List<TaskListDto> dtoList = new ArrayList<>();
         for(Task task : taskList){
-            dtoList.add(TaskListDto.mapToDto(task));
+            dtoList.add(new TaskListDto(task));
         }
         return dtoList;
     }
@@ -44,17 +43,18 @@ public class TaskController {
         var result = getTaskById.execute(id);
         if(result.get().isLeft()){
             var temp = result.get().getLeft();
-            throw new InternalServerErrorDto(temp);
+            throw new NotFoundDto(temp);
         }
         return CompletableFuture.completedFuture(new TaskDto(result.get().get()));
     }
+
     @PutMapping("/{id}")
-    public void updateTask(@PathVariable Integer id) throws ExecutionException, InterruptedException {
+    public void updateTask(@PathVariable("id") Integer id) throws ExecutionException, InterruptedException {
         var task = getTaskById.execute(id).get().get();
         updateTask.execute(task);
     }
     @DeleteMapping("/{id}")
-    public void deleteTaskById(@PathVariable Integer id){
+    public void deleteTaskById(@PathVariable("id") Integer id){
         deleteTaskById.execute(id);
     }
 
