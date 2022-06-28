0ed73adfd34fdeeebed2287d8d2d23494b2fd944
Ilya Voenkov <voenkov@gmail.com>
2022-06-28 15:48:43 +0400

    feat: добавлена поддержка JPA репозиториев

--- src/main/java/finalproject/Main.java
+++ src/main/java/finalproject/Main.java
@@ -2,6 +2,7 @@ package finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories
 public class Main extends AsyncConfigurerSupport {
 
   public static void main(String[] args) {
