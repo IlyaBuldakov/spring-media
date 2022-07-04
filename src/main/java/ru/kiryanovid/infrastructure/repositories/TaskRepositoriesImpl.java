c67149c744bec9647e75af48f4cca80e44b53344
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-07-04 06:50:34 +0400

    fix: изменены класс работы с БД

--- src/main/java/ru/kiryanovid/infrastructure/repositories/TaskRepositoriesImpl.java
+++ src/main/java/ru/kiryanovid/infrastructure/repositories/TaskRepositoriesImpl.java
@@ -8,6 +8,7 @@ import ru.kiryanovid.domain.entity.errors.Failure;
 import ru.kiryanovid.domain.entity.task.Task;
 import ru.kiryanovid.domain.repositories.TaskRepositories;
 import ru.kiryanovid.infrastructure.models.TaskModel;
+import ru.kiryanovid.infrastructure.models.UserModel;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
@@ -20,50 +21,70 @@ public class TaskRepositoriesImpl implements TaskRepositories {
 
     @Override
     public CompletableFuture<Either<Failure, Task>> create(Task task) {
-        TaskModel taskModel = new TaskModel(task.getId(),
+        var x = task.getExecutor();
+        var taskModel = new TaskModel(task.getId(),
                 task.getName(),
                 task.getContentType(),
                 task.getDescription(),
-                task.getAuthor(),
-                task.getExecutor(),
-                task.getDateExpired());
+                null,
+                new UserModel(task.getAuthor()),
+                new UserModel(task.getExecutor()),
+                null,
+                task.getDateExpired(),
+                null,
+                null,
+                null
+                );
         tasks.save(taskModel);
         return null;
     }
 
     @Override
     public Future<Either<Failure, Task>> update(Task task) {
+
         return null;
     }
 
     @Override
     public Future<Either<Failure, Void>> delete(Integer id) {
+        tasks.deleteById(id);
         return null;
     }
 
     @Override
     public CompletableFuture<Either<Failure, Task>> get(Integer id) {
-        var task = tasks.findById(id).get();
-        var task1 = Task.create(null,
-                task.getName(),
-                task.getContentType(),
-                task.getDescription(),
+        var taskModel = tasks.findById(id).orElseThrow();
+        var task = Task.create(null,
+                taskModel.getName(),
+                taskModel.getContentType(),
+                taskModel.getDescription(),
                 null,
-                task.getAuthor(),
-                task.getExecutor(),
+                taskModel.getId(),
+                taskModel.getId(),
                 null,
-                task.getDateExpired(),
+                taskModel.getDateExpired(),
                 null,
                 null,
                 null).get();
 
-        return CompletableFuture.completedFuture(Either.right(task1));
+        return CompletableFuture.completedFuture(Either.right(task));
     }
 
     @Override
     public CompletableFuture<Either<Failure, Iterable<Task>>> getAll() {
-
-        return null;
+        var taskList = tasks.findAll().stream().map(taskModel -> Task.create(null,
+                taskModel.getName(),
+                taskModel.getContentType(),
+                taskModel.getDescription(),
+                null,
+                taskModel.getId(),
+                taskModel.getId(),
+                null,
+                taskModel.getDateExpired(),
+                null,
+                null,
+                null).get()).toList();
+        return CompletableFuture.completedFuture(Either.right(taskList));
 
     }
     public interface Tasks extends JpaRepository<TaskModel, Integer>{
