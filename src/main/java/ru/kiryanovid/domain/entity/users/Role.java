25888675f94dba6a4b940810b3a55db83630e371
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-07-10 10:32:47 +0400

    fix: enum Role настроен для работы с json

--- src/main/java/ru/kiryanovid/domain/entity/users/Role.java
+++ src/main/java/ru/kiryanovid/domain/entity/users/Role.java
@@ -1,16 +1,18 @@
 package ru.kiryanovid.domain.entity.users;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 /**
  *
  */
 public enum Role {
-    ADMIN(1,"administrator"),
+    ADMIN(1,"admin"),
     MANAGER(2, "manager"),
     CONTENT_MAKER(3, "content-maker");
 
     @Getter private final int id;
+    @JsonValue
     @Getter private final String name;
 
     Role(int id, String name){
