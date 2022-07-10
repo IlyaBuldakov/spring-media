78bd0162cbf22bb6fe9f02ba14ce2ae827243cb6
Ilya Voenkov <voenkov@gmail.com>
2022-07-10 23:36:17 +0400

    feat: написан контроллер авторизации

---
+++ src/main/java/finalproject/application/controllers/AuthController.java
@@ -0,0 +1,44 @@
+package finalproject.application.controllers;
+
+import finalproject.application.dto.auth.AuthLoginRequestDto;
+import finalproject.application.dto.auth.AuthLoginResponseDto;
+import finalproject.application.dto.auth.AuthRefreshTokenDto;
+import finalproject.application.services.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/login")
+  public ResponseEntity<AuthLoginResponseDto> login(@RequestBody AuthLoginRequestDto authRequest)
+          throws ExecutionException, InterruptedException {
+    final AuthLoginResponseDto token = authService.login(authRequest);
+    return ResponseEntity.ok(token);
+  }
+
+  @PostMapping("/token")
+  public ResponseEntity<AuthLoginResponseDto> getNewAccessToken(@RequestBody AuthRefreshTokenDto request)
+          throws ExecutionException, InterruptedException {
+    final AuthLoginResponseDto token = authService.getAccessToken(request.getRefreshToken());
+    return ResponseEntity.ok(token);
+  }
+
+  @PostMapping("/refresh-token")
+  public ResponseEntity<AuthLoginResponseDto> getNewRefreshToken(@RequestBody AuthRefreshTokenDto request)
+          throws ExecutionException, InterruptedException {
+    final AuthLoginResponseDto token = authService.refresh(request.getRefreshToken());
+    return ResponseEntity.ok(token);
+  }
+
+}
---
+++ src/main/java/finalproject/configuration/SecurityConfig.java
@@ -0,0 +1,40 @@
+package finalproject.configuration;
+
+import finalproject.application.services.auth.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+  private final JwtFilter jwtFilter;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeHttpRequests(
+                    authz -> authz
+                            .antMatchers("/api/auth/login", "/api/auth/token", "/api/auth/refresh-token", "/api/users",
+                                    "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                            .anyRequest().authenticated()
+                            .and()
+                            .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            ).build();
+  }
+
+}
