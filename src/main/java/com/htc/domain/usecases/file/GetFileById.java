e8797ba4fe55614e4493f4e87241b04cbca65fec
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-11 20:54:09 +0400

    feat: Добавлен сценарий получения файла по идентификатору.

---
+++ src/main/java/com/htc/domain/usecases/file/GetFileById.java
@@ -0,0 +1,38 @@
+package com.htc.domain.usecases.file;
+
+import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.entities.failures.InvalidValues;
+import com.htc.domain.entities.file.File;
+import com.htc.domain.entities.utility.parameters.Id;
+import com.htc.domain.repositories.FileRepository;
+import com.htc.domain.usecases.UseCase;
+import com.htc.utility.EitherHelper;
+import io.vavr.control.Either;
+import java.util.concurrent.CompletableFuture;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Сценарий получения файла по его идентификатору.
+ */
+@Component
+@AllArgsConstructor
+public final class GetFileById implements UseCase<GetFileById.Params, File> {
+  /**
+   * Параметры сценария получения файла по его идентификатору.
+   *
+   * @param id Идентификатор файла.
+   * @param key Ключ идентификатора файла.
+   */
+  public record Params(Long id, String key) {}
+
+  private final FileRepository repository;
+
+  @Override
+  public CompletableFuture<Either<Failure, File>> execute(Params params) {
+    var id = Id.create(params.id());
+    return id.isRight()
+            ? repository.get(id.get())
+            : EitherHelper.badLeft(new InvalidValues());
+  }
+}
