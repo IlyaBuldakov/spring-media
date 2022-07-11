7b4762edb4a592c898b6b8bebba143a3814abdea
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-11 07:44:18 +0400

    feat: Создано представление пользователя в БД

---
+++ src/main/java/com/htc/infrastructure/mappers/UserMapper.java
@@ -0,0 +1,61 @@
+package com.htc.infrastructure.mappers;
+
+import com.htc.domain.entities.user.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author IlyaBuldakov
+ */
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserMapper {
+    public UserMapper(String name, String password, String email, String avatar, String role) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.avatar = avatar;
+        this.role = role;
+    }
+
+    public UserMapper(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.avatar = user.getAvatar();
+        this.role = user
+                .getRole()
+                .getRoleType()
+                .name();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Integer id;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "password")
+    public String password;
+
+    @Column(name = "email")
+    public String email;
+
+    @Column(name = "avatar")
+    public String avatar;
+
+    @Column(name = "role")
+    public String role;
+}
