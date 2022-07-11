66c9d263708678a4e149654627063bc4b75b7b5e
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-11 20:49:24 +0400

    fix: Обновлён репозиторий файла.

--- src/main/java/com/htc/domain/repositories/FileRepository.java
+++ src/main/java/com/htc/domain/repositories/FileRepository.java
@@ -2,6 +2,12 @@ package com.htc.domain.repositories;
 
 import com.htc.domain.entities.failures.Failure;
 import com.htc.domain.entities.file.File;
+import com.htc.domain.entities.file.Format;
+import com.htc.domain.entities.utility.parameters.DateCreated;
+import com.htc.domain.entities.utility.parameters.Id;
+import com.htc.domain.entities.utility.parameters.file.FileBody;
+import com.htc.domain.entities.utility.parameters.file.FileName;
+import com.htc.domain.entities.utility.parameters.file.FileUrlPath;
 import io.vavr.control.Either;
 import java.util.concurrent.CompletableFuture;
 
@@ -12,16 +18,32 @@ public interface FileRepository {
   /**
    * Загрузка нового файла в задачу.
    *
-   * @param file файл
+   * @param name имя файла
+   * @param dateCreated дата создания
+   * @param format форма файла, подробнее {@link Format}
+   * @param fileUrlPath путь до файла
+   * @param file содержимое файла, в формате base64
+   * @return result файл, подробнее {@link File}
+   */
+  CompletableFuture<Either<Failure, File>> upload(FileName name,
+                                                  DateCreated dateCreated,
+                                                  Format format,
+                                                  FileUrlPath fileUrlPath,
+                                                  FileBody file);
+
+  /**
+   * Получение файла.
+   *
+   * @param fileId идентификатор файла
    *
    * @return file файл, подробнее {@link File}
    */
-  CompletableFuture<Either<Failure, File>> upload(File file);
+  CompletableFuture<Either<Failure, File>> get(Id fileId);
 
   /**
    * Удаление файла по идентификатору.
    *
    * @param fileId идентификатор файла
    */
-  CompletableFuture<Either<Failure, Void>> delete(int fileId);
+  CompletableFuture<Either<Failure, Void>> delete(Id fileId);
 }
