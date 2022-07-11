cb2597c1c89eecadce2e8796cadc544134933f19
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-11 06:49:38 +0400

    feat: Написан тест для сервиса

--- src/test/java/com/htc/application/services/impl/UsersServiceImplTest.java
+++ src/test/java/com/htc/application/services/impl/UsersServiceImplTest.java
@@ -1,27 +1,17 @@
 package com.htc.application.services.impl;
 
 import com.htc.application.dto.user.UserRequest;
+import com.htc.application.dto.user.UserResponse;
 import com.htc.domain.entities.user.User;
-import com.htc.domain.repositories.UsersRepository;
-import com.htc.domain.usecases.user.SearchUsers;
 import com.htc.util.Users;
-import io.vavr.control.Either;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
-// Mock-и для проверки вызова соответствующего метода UseCase'а
-import static com.htc.domain.usecases.user.CreateUserTest.mockCreateUser;
-import static com.htc.domain.usecases.user.DeleteUserByIdTest.mockDeleteUserById;
-import static com.htc.domain.usecases.user.GetAllUsersTest.mockGetAllUsers;
-import static com.htc.domain.usecases.user.GetUserByIdTest.mockGetUserById;
-import static com.htc.domain.usecases.user.SearchUsersTest.mockSearchUsers;
-import static com.htc.domain.usecases.user.UpdateUserTest.mockUpdateUser;
-
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -29,84 +19,65 @@ import static org.mockito.Mockito.when;
  */
 class UsersServiceImplTest {
 
-    static UsersServiceImpl usersServiceImpl
-            = new UsersServiceImpl(mockCreateUser, mockGetUserById, mockGetAllUsers,
-            mockUpdateUser, mockDeleteUserById, mockSearchUsers);
-    static UsersRepository usersRepository = mock(UsersRepository.class);
+    String testIdParam = "1";
 
-    static User testUser = Users.createTestUser();
+    User testUser = Users.createTestUser();
 
-    static String testIdParam = "1";
+    UserRequest testUserRequest = new UserRequest(testUser);
 
-    static String testQuery = "QUERY";
+    UsersServiceImpl usersServiceImpl = mock(UsersServiceImpl.class);
 
-    @BeforeAll
-    static void before() {
-        when(usersRepository.getAll()).thenReturn(CompletableFuture.completedFuture(Either.right(List.of(
-                Users.createTestUser(),
-                Users.createTestUser(),
-                Users.createTestUser()
-        ))));
-
-        when(mockGetAllUsers.execute(null)).thenCallRealMethod();
-        when(mockGetAllUsers.setUsersRepository(usersRepository)).thenCallRealMethod();
-
-        mockGetAllUsers.setUsersRepository(usersRepository);
-
-        when(mockGetUserById.execute(testIdParam)).thenCallRealMethod();
-        when(mockGetUserById.setUsersRepository(usersRepository)).thenCallRealMethod();
-
-        mockGetUserById.setUsersRepository(usersRepository);
-
-        when(mockCreateUser.execute(testUser)).thenCallRealMethod();
-        when(mockCreateUser.setUsersRepository(usersRepository)).thenCallRealMethod();
-
-        mockCreateUser.setUsersRepository(usersRepository);
-
-        when(mockUpdateUser.execute(testUser)).thenCallRealMethod();
-        when(mockUpdateUser.setUsersRepository(usersRepository)).thenCallRealMethod();
-
-        mockUpdateUser.setUsersRepository(usersRepository);
-
-        when(mockDeleteUserById.execute(testIdParam)).thenCallRealMethod();
-        when(mockDeleteUserById.setUsersRepository(usersRepository)).thenCallRealMethod();
-
-        mockDeleteUserById.setUsersRepository(usersRepository);
+    @Test
+    void getAllShouldReturnListUserResponse() throws ExecutionException, InterruptedException {
+        when(usersServiceImpl.getAll()).thenReturn(
+                CompletableFuture.completedFuture(
+                        List.of(new UserResponse(testUser),
+                                new UserResponse(testUser),
+                                new UserResponse(testUser))
+                )
+        );
+        var retVal = usersServiceImpl.getAll();
+        assertThat(retVal).isNotNull().isNotCancelled();
+        assertThat(retVal.get().get(0)).isInstanceOf(UserResponse.class);
     }
 
     @Test
-    void getAllUsers_ShouldCallUseCaseMethod() {
-        usersServiceImpl.getAll();
-        verify(mockGetAllUsers).execute(null);
+    void getByIdShouldReturnUserResponse() throws ExecutionException, InterruptedException {
+        when(usersServiceImpl.getById(testIdParam)).thenReturn(
+                CompletableFuture.completedFuture(new UserResponse(testUser))
+        );
+        var retVal = usersServiceImpl.getById(testIdParam);
+        assertThat(retVal).isNotNull().isNotCancelled();
+        assertThat(retVal.get()).isInstanceOf(UserResponse.class);
     }
 
     @Test
-    void getUserById_ShouldCallUseCaseMethod() {
-        usersServiceImpl.getById(testIdParam);
-        verify(mockGetUserById).execute(testIdParam);
+    void createShouldReturnUserResponse() throws ExecutionException, InterruptedException {
+        when(usersServiceImpl.create(testUserRequest)).thenReturn(
+                CompletableFuture.completedFuture(new UserResponse(testUser))
+        );
+        var retVal = usersServiceImpl.create(testUserRequest);
+        assertThat(retVal).isNotNull().isNotCancelled();
+        assertThat(retVal.get()).isInstanceOf(UserResponse.class);
     }
 
     @Test
-    void createUser_ShouldCallUseCaseMethod() {
-        usersServiceImpl.create(new UserRequest(testUser));
-        verify(mockCreateUser).execute(testUser);
+    void updateShouldReturnUserResponse() throws ExecutionException, InterruptedException {
+        when(usersServiceImpl.update(testUserRequest, testIdParam)).thenReturn(
+                CompletableFuture.completedFuture(new UserResponse(testUser))
+        );
+        var retVal = usersServiceImpl.update(testUserRequest, testIdParam);
+        assertThat(retVal).isNotNull().isNotCancelled();
+        assertThat(retVal.get()).isInstanceOf(UserResponse.class);
     }
 
     @Test
-    void updateUser_ShouldCallUseCaseMethod() {
-        usersServiceImpl.update(new UserRequest(testUser));
-        verify(mockUpdateUser).execute(testUser);
-    }
-
-    @Test
-    void deleteUser_ShouldCallUseCaseMethod() {
-        usersServiceImpl.delete(testIdParam);
-        verify(mockDeleteUserById).execute(testIdParam);
-    }
-
-    @Test
-    void searchUser_ShouldCallUseCaseMethod() {
-        usersServiceImpl.search(testQuery);
-        verify(mockSearchUsers).execute(new SearchUsers.Query(testQuery, null));
+    void deleteShouldReturnUserResponse() throws ExecutionException, InterruptedException {
+        when(usersServiceImpl.delete(testIdParam)).thenReturn(
+                CompletableFuture.completedFuture(new UserResponse(testUser))
+        );
+        var retVal = usersServiceImpl.delete(testIdParam);
+        assertThat(retVal).isNotNull().isNotCancelled();
+        assertThat(retVal.get()).isInstanceOf(UserResponse.class);
     }
 }
