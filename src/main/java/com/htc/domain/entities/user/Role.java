3d7a02bedaaa22d5a1a348f6e2c5254aef967d5d
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-11 07:12:57 +0400

    feat: Добавлен конструктор с валидацией для роли

--- src/main/java/com/htc/domain/entities/user/Role.java
+++ src/main/java/com/htc/domain/entities/user/Role.java
@@ -1,5 +1,7 @@
 package com.htc.domain.entities.user;
 
+import com.htc.domain.entities.failures.Failure;
+import io.vavr.control.Either;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,9 +10,30 @@ import lombok.Getter;
  *
  * @author IlyaBuldakov
  */
-
 @AllArgsConstructor
 public class Role {
+
+    /**
+     * Конструктор, превращающий строку из запроса в Role
+     *
+     * @param input Строка
+     */
+    public Role(String input) {
+        RoleType type = validateRoleTypeInput(input).get();
+        this.id = type.ordinal() + 1;
+        this.roleType = type;
+    }
+
+    /**
+     * Метод валидации строки из запроса
+     * на соответствие RoleType'у
+     *
+     * @param input Строка для валидации
+     * @return Failure или RoleType
+     */
+    public static Either<Failure, RoleType> validateRoleTypeInput(String input) {
+        return Either.right(RoleType.valueOf(input.toUpperCase()));
+    }
 
     /**
      * Тип роли
@@ -45,5 +68,4 @@ public class Role {
      * @return roleType Тип роли
      */
     private @Getter RoleType roleType;
-
 }
