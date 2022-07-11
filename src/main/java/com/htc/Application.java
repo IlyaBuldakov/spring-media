2b0bfacbb38c8309e3a58ab416999f48bc8a5642
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-11 07:46:34 +0400

    feat: Конфигурация проекта

--- src/main/java/com/htc/Application.java
+++ src/main/java/com/htc/Application.java
@@ -2,6 +2,7 @@ package com.htc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
