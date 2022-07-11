0ce6b09f38a5c7bb3db152d59556f9bd136d62bc
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-11 20:53:04 +0400

    fix: Обновлены сценарии использования файла.

--- src/main/java/com/htc/domain/usecases/file/DeleteFileById.java
+++ src/main/java/com/htc/domain/usecases/file/DeleteFileById.java
@@ -1,8 +1,11 @@
 package com.htc.domain.usecases.file;
 
 import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.entities.failures.InvalidValues;
+import com.htc.domain.entities.utility.parameters.Id;
 import com.htc.domain.repositories.FileRepository;
 import com.htc.domain.usecases.UseCase;
+import com.htc.utility.EitherHelper;
 import io.vavr.control.Either;
 import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
@@ -13,11 +16,22 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @AllArgsConstructor
-public final class DeleteFileById implements UseCase<Integer, Void> {
+public final class DeleteFileById implements UseCase<DeleteFileById.Params, Void> {
+  /**
+   * Параметры сценария удаления файла по его идентификатору.
+   *
+   * @param id Идентификатор пользователя.
+   * @param key Ключ идентификатора пользователя.
+   */
+  public record Params(Long id, String key) {}
+
   private final FileRepository repository;
 
   @Override
-  public CompletableFuture<Either<Failure, Void>> execute(Integer id) {
-    return repository.delete(id);
+  public CompletableFuture<Either<Failure, Void>> execute(Params params) {
+    var id = Id.create(params.id());
+    return id.isRight()
+            ? repository.delete(id.get())
+            : EitherHelper.badLeft(new InvalidValues());
   }
 }
--- src/main/java/com/htc/domain/usecases/file/UploadFile.java
+++ src/main/java/com/htc/domain/usecases/file/UploadFile.java
@@ -1,9 +1,17 @@
 package com.htc.domain.usecases.file;
 
 import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.entities.failures.InvalidValueParam;
+import com.htc.domain.entities.failures.InvalidValues;
 import com.htc.domain.entities.file.File;
+import com.htc.domain.entities.file.Format;
+import com.htc.domain.entities.utility.parameters.DateCreated;
+import com.htc.domain.entities.utility.parameters.file.FileBody;
+import com.htc.domain.entities.utility.parameters.file.FileName;
+import com.htc.domain.entities.utility.parameters.file.FileUrlPath;
 import com.htc.domain.repositories.FileRepository;
 import com.htc.domain.usecases.UseCase;
+import com.htc.utility.EitherHelper;
 import io.vavr.control.Either;
 import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
@@ -14,11 +22,46 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @AllArgsConstructor
-public final class UploadFile implements UseCase<File, File> {
+public final class UploadFile implements UseCase<UploadFile.Params, File> {
+  /**
+   * Параметры сценария добавления файла.
+   */
+
+  public record Params(String name, String nameKey,
+                       Format format, String formatKey,
+                       String fileUrlPath, String fileUrlPathKey,
+                       String file, String fileKey) {}
+
   private final FileRepository repository;
 
   @Override
-  public CompletableFuture<Either<Failure, File>> execute(File file) {
-    return repository.upload(file);
+  public CompletableFuture<Either<Failure, File>> execute(Params params) {
+    var failure = new InvalidValues();
+    var name = FileName.create(params.name());
+    if (name.isLeft()) {
+      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_NAME, params.nameKey);
+    }
+    var dateCreated = DateCreated.create();
+    if (dateCreated.isLeft()) {
+      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_DATE_CREATED, "Date created");
+    }
+    var fileUrlPath = FileUrlPath.create(params.fileUrlPath());
+    if (fileUrlPath.isLeft()) {
+      failure.getValues().put(InvalidValueParam.INVALID_FILE_URL_PATH, params.fileUrlPathKey);
+    }
+    var file = FileBody.create(params.file());
+    if (file.isLeft()) {
+      failure.getValues().put(InvalidValueParam.INVALID_FILE_BODY, params.fileKey);
+    }
+
+    return failure.getValues().size() == 0
+            ? repository.upload(
+                    name.get(),
+                    dateCreated.get(),
+                    params.format(),
+                    fileUrlPath.get(),
+                    file.get()
+              )
+            : EitherHelper.badLeft(failure);
   }
 }
