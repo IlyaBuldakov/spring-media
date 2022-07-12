58aff1b28d44b909531debf716936f2d72470747
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-07-12 11:12:48 +0400

    fix: Доработан обработчик исключений.

--- src/main/java/com/htc/application/controllers/ApplicationExceptionHandler.java
+++ src/main/java/com/htc/application/controllers/ApplicationExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.htc.application.controllers;
 
+import com.fasterxml.jackson.core.JacksonException;
 import com.htc.application.dto.responsestatus.ApplicationFailureException;
+import com.htc.application.dto.responsestatus.BadRequestResponse;
+import com.htc.domain.entities.failures.InvalidValues;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +16,17 @@ public class ApplicationExceptionHandler {
   @ExceptionHandler(ApplicationFailureException.class)
   ResponseEntity<ApplicationFailureException> handleAuth(ApplicationFailureException exception) {
     return new ResponseEntity<>(exception, exception.getStatus());
+  }
+
+  @ExceptionHandler({
+      IllegalArgumentException.class,
+      JacksonException.class
+  })
+  ResponseEntity<ApplicationFailureException> handleIllegalArgument(
+          Exception exception) {
+
+    InvalidValues invalidValues = new InvalidValues();
+    var applicationFailure = new BadRequestResponse(invalidValues);
+    return new ResponseEntity<>(applicationFailure, applicationFailure.getStatus());
   }
 }
