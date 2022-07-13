fd379553afee6c9a453b5b7e46891aa5245e6a51
Ilya Voenkov <voenkov@gmail.com>
2022-07-13 16:22:14 +0400

    feat: создана заготовка для класса, имплементирующего сервис задач.

---
+++ src/main/java/finalproject/application/services/impl/TaskServiceImpl.java
@@ -0,0 +1,56 @@
+package finalproject.application.services.impl;
+
+import finalproject.application.services.TaskService;
+import finalproject.domain.entities.failures.Failure;
+import finalproject.domain.entities.task.Task;
+import finalproject.domain.repositories.*;
+import io.vavr.control.Either;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.Future;
+
+@Service
+@AllArgsConstructor
+public class TaskServiceImpl implements TaskService {
+
+  private final UserRepository userRepository;
+  private final TaskRepository taskRepository;
+  private final ContentRepository contentRepository;
+  private final CommentRepository commentRepository;
+  private final FileRepository fileRepository;
+
+
+
+  @Override
+  public Future<Either<Failure, Task>> createNewTask(Task task) {
+    return null;
+  }
+
+  @Override
+  public Future<Either<Failure, Task>> editTask(Task task, int id) {
+    return null;
+  }
+
+  @Override
+  public Future<Either<Failure, Void>> deleteTask(Task task, int id) {
+    return null;
+  }
+
+  @Override
+  public Future<Either<Failure, Task>> getTaskById(int id) {
+    return null;
+  }
+
+  @Override
+  public Future<Either<Failure, List<Task>>> getAllTasks() {
+    return null;
+  }
+
+  @Override
+  public Future<Either<Failure, Task>> approveTask(Task task) {
+    return null;
+  }
+}
+
