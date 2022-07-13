588ce55b1370bde4f0fbca6edc726da68a9bbf5f
Ilya Voenkov <voenkov@gmail.com>
2022-07-13 16:23:33 +0400

    fix: исправлен репозиторий задач

--- src/main/java/finalproject/domain/repositories/TaskRepository.java
+++ src/main/java/finalproject/domain/repositories/TaskRepository.java
@@ -1,9 +1,9 @@
 package finalproject.domain.repositories;
 
-import finalproject.domain.entities.user.User;
+import finalproject.domain.entities.task.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends CrudRepository<User, Integer> {
+public interface TaskRepository extends CrudRepository<Task, Integer> {
 }
