89e29f95ac9a33b796c2e4779cdca74ba4667d2e
Ilya Voenkov <voenkov@gmail.com>
2022-07-13 16:22:40 +0400

    fix: доработан интерфейс сервиса задач

--- src/main/java/finalproject/application/services/TaskService.java
+++ src/main/java/finalproject/application/services/TaskService.java
@@ -10,13 +10,11 @@ import java.util.concurrent.Future;
 
 public interface TaskService {
 
-  Future<Either<Failure, Task>> createNewTask(Task task, User user);
-  Future<Either<Failure, Task>> editTask(Task task, User user);
-  Future<Either<Failure, Void>> deleteTask(Task task);
+  Future<Either<Failure, Task>> createNewTask(Task task);
+  Future<Either<Failure, Task>> editTask(Task task, int id);
+  Future<Either<Failure, Void>> deleteTask(Task task, int id);
   Future<Either<Failure, Task>> getTaskById(int id);
-  Future<Either<Failure, List<Task>>> getAllTasks(User user);
-  Future<Either<Failure, List<Task>>> getTasksByQuery(String query, User user);
-
+  Future<Either<Failure, List<Task>>> getAllTasks();
   /**
    * статус задачи меняется с FEEDBACK на APPROVED, статус контента меняется с NON-PUBLISHED на PUBLISHED
    */
