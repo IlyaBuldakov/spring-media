ddd8cc312b23686ff1e676e1550082a1cd59cc67
Ilya Voenkov <voenkov@gmail.com>
2022-07-13 16:25:36 +0400

    fix: доработан класс перечисления типов контента.

--- src/main/java/finalproject/domain/entities/content/ContentType.java
+++ src/main/java/finalproject/domain/entities/content/ContentType.java
@@ -1,16 +1,28 @@
 package finalproject.domain.entities.content;
 
+import finalproject.domain.entities.user.Role;
+import lombok.Getter;
+
+import java.util.HashMap;
+
 public enum ContentType {
-  /**
-   * Видео.
-   */
-  VIDEO,
-  /**
-   * Аудио.
-   */
-  AUDIO,
-  /**
-   * Фото.
-   */
-  PHOTO
+  VIDEO(0, "video"),
+  AUDIO(1, "audio"),
+  PHOTO(2, "photo");
+  final @Getter int id;
+  final @Getter String name;
+
+  ContentType(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+  static final HashMap<String, ContentType> map = new HashMap<>();
+  static {
+    for (ContentType contentType : ContentType.values()) {
+      map.put(contentType.getName(), contentType);
+    }
+  }
+  public static ContentType getContentTypeByName(String name) {
+    return map.get(name);
+  }
 }
