496e30b748b05d89a79a660b7ec9d598f3707e1f
Ilya Voenkov <voenkov@gmail.com>
2022-07-13 16:17:17 +0400

    fix: убран ненужный конструктор

--- src/main/java/finalproject/application/dto/user/UserDto.java
+++ src/main/java/finalproject/application/dto/user/UserDto.java
@@ -1,13 +1,12 @@
 package finalproject.application.dto.user;
 
 import finalproject.domain.entities.user.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Пользователь.
  */
-@AllArgsConstructor
+
 public class UserDto extends UserBasicDto {
 
   /**
@@ -15,21 +14,21 @@ public class UserDto extends UserBasicDto {
    *
    * @return id Электронная почта пользователя.
    */
-  private @Getter String email;
+  private final @Getter String email;
 
   /**
    * Изображение пользователя.
    *
    * @return id Изображение пользователя.
    */
-  private @Getter String avatar;
+  private final @Getter String avatar;
 
   /**
    * Роль пользователя, см. {@link RoleDto}.
    *
    * @return id Роль пользователя.
    */
-  private @Getter RoleDto role;
+  private final @Getter RoleDto role;
 
   public UserDto(User user) {
     super(user.getId(), user.getName());
