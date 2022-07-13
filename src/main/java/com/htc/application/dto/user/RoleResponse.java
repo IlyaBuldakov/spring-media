6bd32eb05d36d93d03ba5b97aec52a5d1c3aa7c3
Yaroslav <yaroslav-w@mail.ru>
2022-07-13 13:42:26 +0400

    feat: Представление роли пользователя (ответ на запрос).

---
+++ src/main/java/com/htc/application/dto/user/RoleResponse.java
@@ -0,0 +1,35 @@
+package com.htc.application.dto.user;
+
+import com.htc.domain.entities.User;
+import lombok.Getter;
+
+/**
+ * Представление роли пользователя (ответ на запрос).
+ */
+public class RoleResponse {
+  /**
+   * Идентификатор роли.
+   *
+   * @return id Идентификатор роли.
+   */
+  @SuppressWarnings("JavadocDeclaration")
+  private final @Getter int id;
+
+  /**
+   * Название роли.
+   *
+   * @return name Название роли.
+   */
+  @SuppressWarnings("JavadocDeclaration")
+  private final @Getter String name;
+
+  /**
+   * Создаёт экземпляр класса {@link RoleResponse}.
+   *
+   * @param role Роль пользователя.
+   */
+  public RoleResponse(User.Role role) {
+    this.id = role.getId();
+    this.name = role.getName();
+  }
+}
