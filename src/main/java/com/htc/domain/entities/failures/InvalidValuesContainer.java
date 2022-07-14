5e8a73d9dda81a8eba1a105d795c2102eb992c0a
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-14 10:13:56 +0400

    feat: Добавлен класс-контейнер для ошибок InvalidValue

---
+++ src/main/java/com/htc/domain/entities/failures/InvalidValuesContainer.java
@@ -0,0 +1,36 @@
+package com.htc.domain.entities.failures;
+
+import lombok.Getter;
+
+import java.util.LinkedList;
+
+/**
+ * Класс, содержащий связку InvalidValue.
+ */
+public class InvalidValuesContainer implements Failure {
+    /**
+     * Список ошибок для использования в BadRequest
+     * (см. спецификацию).
+     *
+     * @return Список ошибок.
+     */
+    private final @Getter LinkedList<InvalidValue> invalidValues = new LinkedList<>();
+
+    /**
+     * Добавление InvalidValue в {@link InvalidValuesContainer#getInvalidValues() список}.
+     *
+     * @param invalidValue Ошибка.
+     */
+    public void addInvalidValue(InvalidValue invalidValue) {
+        invalidValues.add(invalidValue);
+    }
+
+    /**
+     * Слияние контейнеров {@link InvalidValue}.
+     *
+     * @param container Другой контейнер.
+     */
+    public void merge(InvalidValuesContainer container) {
+        invalidValues.addAll(container.invalidValues);
+    }
+}
