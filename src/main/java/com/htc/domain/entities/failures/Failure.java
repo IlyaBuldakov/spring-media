1ca9ef9d51df882545135da177ff16bd330dd4a4
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-14 10:07:01 +0400

    fix: Failure переделан в маркерный интерфейс

--- src/main/java/com/htc/domain/entities/failures/Failure.java
+++ src/main/java/com/htc/domain/entities/failures/Failure.java
@@ -4,11 +4,4 @@ package com.htc.domain.entities.failures;
  * Интерфейс, описывающий сущность ошибки.
  */
 public interface Failure {
-
-    /**
-     * Получение сообщения об ошибке.
-     *
-     * @return Сообщение об ошибке.
-     */
-    String getMessage();
 }
