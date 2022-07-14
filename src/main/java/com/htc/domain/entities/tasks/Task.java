c3cf948f17b98d6b6272ba0476cc58618dfd598a
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-07-14 15:17:19 +0400

    fix: Класс Task преобразован в интерфейс.

--- src/main/java/com/htc/domain/entities/tasks/Task.java
+++ src/main/java/com/htc/domain/entities/tasks/Task.java
@@ -1,156 +1,111 @@
 package com.htc.domain.entities.tasks;
 
-import com.htc.application.dto.file.FileDto;
 import com.htc.domain.entities.attributes.Attribute;
 import com.htc.domain.entities.attributes.Id;
 import com.htc.domain.entities.comments.Comment;
 import com.htc.domain.entities.content.Content;
 import com.htc.domain.entities.content.ContentType;
-import com.htc.domain.entities.failures.Failure;
 import com.htc.domain.entities.failures.InvalidValue;
 import com.htc.domain.entities.files.File;
 import com.htc.domain.entities.user.User;
 import io.vavr.control.Either;
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.Random;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * Задачи.
+ * Задача.
  */
-@AllArgsConstructor
-public class Task {
+public interface Task {
   /**
    * Индентификатор задачи.
    *
    * @return Индентификатор задачи.
    */
-  private @Getter Id id;
+  Id getId();
+
   /**
    * Заголовок задачи.
    *
    * @return заголовок задачи.
    */
-  private @Getter Name name;
+  Name getName();
+
   /**
    * Требуемный тип контента.
    *
    * @return Тип контента.
    */
-  private @Getter ContentType contentType;
+  ContentType getContentType();
+
   /**
    * Описание задачи.
    *
    * @return Описание задачи.
    */
-  private @Getter Description description;
+  Description getDescription();
+
   /**
    * Приложеные файлы..
    *
    * @return Файлы задачи.
    */
-  private @Getter Collection<File> files;
+  Collection<File> getFiles();
+
   /**
    * Автор задачи.
    *
    * @return Позьзователя - автора задачи.
    */
-  private @Getter User author;
+  User getAuthor();
+
   /**
    * Исполнитель задачи.
    *
    * @return Позьзователя - исполнителя задачи.
    */
-  private @Getter User executor;
+  User getExecutor();
+
   /**
    * Дата создания задачи.
    *
    * @return Дату создания задачи.
    */
-  private @Getter LocalDateTime dateCreated;
+  LocalDateTime getDateCreated();
+
   /**
    * Срок выполнения задачи.
    *
    * @return Срок выполнения задачи.
    */
-  private @Getter LocalDateTime dateExpired;
+  LocalDateTime getDateExpired();
+
   /**
    * Контент - результат выполнения задачи.
    *
    * @return Контент.
    */
-  private @Getter Collection<Content> contents;
+
+  Collection<Content> getContents();
+
   /**
    * Коментарии задачи.
    *
    * @return Коментарии задачи.
    */
-  private @Getter Collection<Comment> comments;
+  Collection<Comment> getComments();
+
   /**
    * Статус задачи.
    *
    * @return Статус задачи.
    */
-  private @Getter TaskStatus status;
-
-
-  private Task() {
-  }
-
-
-  /**
-   * Создает задачу.
-   *
-   * @param id Индентификатор задачи.
-   * @param name Название задачи.
-   * @param contentType Тип контента.
-   * @param description Описание задачи.
-   * @param files Файлы задачи.
-   * @param author Пользователь - автор задачи.
-   * @param executor Пользователь - исполнитель задачи.
-   * @param dateCreated Дата саздания задачи.
-   * @param dateExpired Срок выполнения задачи.
-   * @param contents Медиаконтент.
-   * @param comments Комментарии.
-   * @param status Статус задачи.
-   * @return Задача, либо ошибку создания задачи.
-   */
-  public static Either<Failure, Task> create(
-          Id id,
-          Name name,
-          ContentType contentType,
-          Description description,
-          Collection<File> files,
-          User author,
-          User executor,
-          LocalDateTime dateCreated,
-          LocalDateTime dateExpired,
-          Collection<Content> contents,
-          Collection<Comment> comments,
-          TaskStatus status) {
-
-    var task = new Task();
-    task.id = id;
-    task.name = name;
-    task.contentType = contentType;
-    task.description = description;
-    task.files = files;
-    task.author = author;
-    task.executor = executor;
-    task.dateCreated = dateCreated;
-    task.dateExpired = dateExpired;
-    task.contents = contents;
-    task.comments = comments;
-    task.status = status;
-    return Either.right(task);
-  }
+  TaskStatus getStatus();
 
   /**
    * Название задачи.
    */
-  public static final class Name extends Attribute<String> {
+  final class Name extends Attribute<String> {
 
     /**
      * Проверяет входные данные на корректность и создаёт название задачи.
@@ -167,7 +122,7 @@ public class Task {
       var taskName = new Task.Name(value);
       return Either.right(taskName);
     }
-    
+
     private Name(String value) {
       super(value);
     }
@@ -177,7 +132,7 @@ public class Task {
   /**
    * Описание задачи.
    */
-  public static final class Description extends Attribute<String> {
+  final class Description extends Attribute<String> {
     /**
      * Проверяет входные данные на корректность и создаёт описание задачи.
      * Описание задачи не должно быть пустой строкой.
@@ -202,7 +157,7 @@ public class Task {
   /**
    * Статус задачи.
    */
-  public enum TaskStatus {
+  enum TaskStatus {
     /**
      * В работе.
      */
