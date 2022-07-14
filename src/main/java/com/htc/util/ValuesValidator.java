76e9fee8b73c3193186e0f5139cf67802a48a287
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-14 10:20:49 +0400

    fix: Разделён метод ValuesValidator

--- src/main/java/com/htc/util/ValuesValidator.java
+++ src/main/java/com/htc/util/ValuesValidator.java
@@ -30,14 +30,29 @@ public class ValuesValidator {
     public static InvalidValuesContainer checkUserFields(
             String id, String name, String password, String email, String avatar) {
         InvalidValuesContainer invalidValues = new InvalidValuesContainer();
-        IntegerValidator integerValidator = IntegerValidator.getInstance();
-        if (!integerValidator.isValid(id)) {
-            invalidValues.addInvalidValue(InvalidValue.INCORRECT_ID);
+        var validateId = validateStringId(id);
+        if (validateId != null) {
+            invalidValues = validateId;
         }
-        int paramToInt = Integer.parseInt(id);
-        if (!integerValidator.minValue(paramToInt, 1)) {
-            invalidValues.addInvalidValue(InvalidValue.NEGATIVE_ID);
+        var anotherFields = checkUserFields(name, password, email, avatar);
+        if (anotherFields != null) {
+            invalidValues.merge(anotherFields);
         }
+        return invalidValues.getInvalidValues().size() == 0 ? null : invalidValues;
+    }
+
+    /**
+     * Метод валидации всех полей пользователя кроме идентификатора.
+     *
+     * @param name     Имя пользователя.
+     * @param password Пароль.
+     * @param email    Электронная почта.
+     * @param avatar   Аватар.
+     * @return {@link InvalidValuesContainer} - контейнер с ошибками {@link InvalidValue}.
+     */
+    public static InvalidValuesContainer checkUserFields(
+            String name, String password, String email, String avatar) {
+        InvalidValuesContainer invalidValues = new InvalidValuesContainer();
         if (name.length() == 0) {
             invalidValues.addInvalidValue(InvalidValue.INCORRECT_USERNAME);
         }
@@ -52,6 +67,25 @@ public class ValuesValidator {
         }
         if (!avatar.matches(BASE64_REGEX) || avatar.length() == 0) {
             invalidValues.addInvalidValue(InvalidValue.INCORRECT_AVATAR);
+        }
+        return invalidValues.getInvalidValues().size() == 0 ? null : invalidValues;
+    }
+
+    /**
+     * Метод валидации идентификатора типа String.
+     *
+     * @param id Идентификатор.
+     * @return {@link InvalidValuesContainer} - контейнер с ошибками {@link InvalidValue}.
+     */
+    public static InvalidValuesContainer validateStringId(String id) {
+        InvalidValuesContainer invalidValues = new InvalidValuesContainer();
+        IntegerValidator integerValidator = IntegerValidator.getInstance();
+        if (!integerValidator.isValid(id)) {
+            invalidValues.addInvalidValue(InvalidValue.INCORRECT_ID);
+        }
+        int paramToInt = Integer.parseInt(id);
+        if (!integerValidator.minValue(paramToInt, 1)) {
+            invalidValues.addInvalidValue(InvalidValue.NEGATIVE_ID);
         }
         return invalidValues.getInvalidValues().size() == 0 ? null : invalidValues;
     }
