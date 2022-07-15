154c0f9ab18335cc4f6ec491fd0140ac1f68cb74
Yaroslav <yaroslav-w@mail.ru>
2022-07-15 15:54:25 +0400

    feat: Метод создания уведомления.

--- src/main/java/com/htc/domain/repositories/NotificationRepository.java
+++ src/main/java/com/htc/domain/repositories/NotificationRepository.java
@@ -1,11 +1,12 @@
 package com.htc.domain.repositories;
 
+import com.htc.domain.entities.Notification;
 import com.htc.domain.entities.failures.Failure;
-import com.htc.domain.entities.notification.Notification;
 import io.vavr.control.Either;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 import org.springframework.stereotype.Repository;
-
-import java.util.concurrent.Future;
 
 /**
  * Репозиторий уведомлений.
@@ -13,16 +14,27 @@ import java.util.concurrent.Future;
 @Repository
 public interface NotificationRepository {
   /**
-   * Создаёт уведомление.
+   * Создает уведомление.
    *
-   * @param notification Уведомление.
+   * @param notificationType Тип уведомления.
+   * @param dateCreateNotification Дата создания уведомления.
+   * @param notificationMessage Сообщение уведомления.
+   * @param userId Иденификатор пользователя для которого уведомление.
+   * @param taskId Идентификатор задачи, которой принадлежит уведомление.
+   * @return Уведомление или ошибка.
    */
-//  Future<Either<Failure, Notification>> create(Notification notification);
+  CompletableFuture<Either<Failure, Notification>> create(
+      Notification.NotificationType notificationType,
+      LocalDateTime dateCreateNotification,
+      String notificationMessage,
+      int userId,
+      int taskId
+  );
 
   /**
    * Получает список всех уведомлений.
    *
    * @return Список всех уведомлений.
    */
-  Future<Either<Failure, Iterable<Notification>>> getAll();
+  CompletableFuture<Either<Failure, Collection<Notification>>> getAll();
 }
