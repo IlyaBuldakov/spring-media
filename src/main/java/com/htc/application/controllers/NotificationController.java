e13d90d0fec6f8aa9a69333506e3ff7c416f6b74
Yaroslav <yaroslav-w@mail.ru>
2022-07-15 16:02:56 +0400

    fix: Контроллер для работы с сущностью Уведомления переработан для работы с СУБД.

--- src/main/java/com/htc/application/controllers/NotificationController.java
+++ src/main/java/com/htc/application/controllers/NotificationController.java
@@ -1,35 +1,40 @@
 package com.htc.application.controllers;
 
-import com.htc.domain.entities.notification.Notification;
+import com.htc.application.dto.notification.NotificationResponse;
 import com.htc.domain.usecases.notification.GetAllNotifications;
-import com.htc.infrastructure.models.user.NotificationModel;
-import com.htc.infrastructure.repositories.JpaNotificationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.htc.utility.Controllers;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
 
 /**
  * Контроллер для работы с уведомлениями.
  */
 @RestController
 @RequestMapping(path = "api/notifications")
+@AllArgsConstructor
 public class NotificationController {
-
-  @Autowired
-  GetAllNotifications getAllNotifications;
+  private GetAllNotifications getAllNotifications;
 
   /**
-   * Возвращает список всех уведомлений.
+   * Получает список уведомлений.
    *
    * @return Список уведомлений.
    */
   @GetMapping
-  public Iterable<Notification> getAll() throws ExecutionException, InterruptedException {
-    return getAllNotifications.execute(null)
-        .get()
-        .get();
+  @Operation(summary = "Получить список уведомлений.")
+  @Async
+  public CompletableFuture<Iterable<NotificationResponse>> getAll() {
+    return Controllers.handleRequest(
+        getAllNotifications,
+        null,
+        users -> users.stream()
+            .map(NotificationResponse::new)
+            .collect(Collectors.toList()));
   }
 }
