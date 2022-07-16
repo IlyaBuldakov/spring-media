7737b1b4a775a75bae12694f1f2f07d838aa7bca
Yaroslav <yaroslav-w@mail.ru>
2022-07-16 22:30:30 +0400

    fix: Создана заготовка для сценария получения всех комментариев по идентификатору задачи и удален фейк-репозиторий.

--- src/main/java/com/htc/domain/usecases/comment/GetCommentsByTaskId.java
+++ src/main/java/com/htc/domain/usecases/comment/GetCommentsByTaskId.java
@@ -1,24 +1,38 @@
 package com.htc.domain.usecases.comment;
 
-import com.htc.domain.entities.comment.Comment;
+import com.htc.domain.entities.Comment;
 import com.htc.domain.entities.failures.Failure;
 import com.htc.domain.repositories.CommentRepository;
 import com.htc.domain.usecases.UseCase;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+// TODO Реализовать механизм получения комментариев по id задачи
+//  – сейчас все существующие комментарии.
+// TODO Реализовать сортировку комментариев по дате создания.
 /**
- * Сценарий получения комментария по его идентификатору.
+ * Сценарий получения комментариев по идентификатору задачи.
  */
 @Component
 @AllArgsConstructor
-public class GetCommentsByTaskId implements UseCase<Integer, Iterable<Comment>> {
+public class GetCommentsByTaskId implements
+    UseCase<GetCommentsByTaskId.Params,
+    Collection<Comment>> {
+  /**
+   * Параметры сценария получения комментариев по идентификатору задачи.
+   *
+   * @param taskId Идентификатор задачи.
+   * @param key Ключ идентификатора задачи.
+   */
+  public record Params(int taskId, String key) {}
+
   private final CommentRepository repository;
 
   @Override
-  public Future<Either<Failure, Iterable<Comment>>> execute(Integer taskId) {
-    return repository.getCommentsByTaskId(taskId);
+  public CompletableFuture<Either<Failure, Collection<Comment>>> execute(Params params) {
+    return repository.getCommentsByTaskId(params.taskId);
   }
 }
