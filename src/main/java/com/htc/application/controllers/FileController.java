10e7e2112407bc23b43b29067657b39cb829035b
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-16 18:29:59 +0400

    fix: Исправлена ссылка на аннотацию RequestBody.

--- src/main/java/com/htc/application/controllers/FileController.java
+++ src/main/java/com/htc/application/controllers/FileController.java
@@ -7,7 +7,6 @@ import com.htc.domain.usecases.file.GetFileById;
 import com.htc.domain.usecases.file.UploadFile;
 import com.htc.utility.ControllerHelper;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/htc/application/controllers/UserController.java
+++ src/main/java/com/htc/application/controllers/UserController.java
@@ -9,7 +9,6 @@ import com.htc.domain.usecases.user.GetUserById;
 import com.htc.domain.usecases.user.UpdateUserById;
 import com.htc.utility.ControllerHelper;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
