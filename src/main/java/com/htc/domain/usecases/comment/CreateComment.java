bf8627a776e4e81c72295c134596319e6958270a
Yaroslav <yaroslav-w@mail.ru>
2022-07-16 22:29:06 +0400

    fix: Переработан сценарий создания сущности комментарий.

--- src/main/java/com/htc/domain/usecases/comment/CreateComment.java
+++ src/main/java/com/htc/domain/usecases/comment/CreateComment.java
@@ -1,11 +1,14 @@
 package com.htc.domain.usecases.comment;
 
-import com.htc.domain.entities.comment.Comment;
+import com.htc.domain.entities.Comment;
 import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.entities.failures.InvalidValues;
 import com.htc.domain.repositories.CommentRepository;
 import com.htc.domain.usecases.UseCase;
+import com.htc.utility.Results;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +17,34 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @AllArgsConstructor
-public class CreateComment implements UseCase<Comment, Comment> {
+public final class CreateComment implements UseCase<CreateComment.Params, Comment> {
+
+  /**
+   * Параметры сценария создания комментария.
+   *
+   * @param dateCreateComment Дата создания комментария.
+   * @param userId Пользователь создавший комментарий.
+   * @param taskId Задача содержащая комментарий.
+   * @param commentMessage Сообщение комментария.
+   */
+  public record Params(
+      LocalDateTime dateCreateComment,
+      int userId,
+      int taskId,
+      String commentMessage) {}
+
   private final CommentRepository repository;
 
   @Override
-  public Future<Either<Failure, Comment>> execute(Comment comment) {
-    return repository.create(comment);
+  public CompletableFuture<Either<Failure, Comment>> execute(Params params) {
+    var failure = new InvalidValues();
+
+    return failure.invalidValues().size() == 0
+        ? repository.create(
+          params.dateCreateComment,
+          params.userId,
+          params.taskId,
+          params.commentMessage)
+        : Results.fail(failure);
   }
 }
