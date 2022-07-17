211195347fa799596dc1dbb260cc222764721718
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-17 18:07:27 +0400

    fix: Сущность контента изменена с класса на интерфейс.

--- src/main/java/com/htc/domain/entities/content/Content.java
+++ src/main/java/com/htc/domain/entities/content/Content.java
@@ -1,88 +1,68 @@
 package com.htc.domain.entities.content;
 
 import com.htc.domain.entities.file.File;
-import com.htc.domain.entities.file.Format;
 import com.htc.domain.entities.user.User;
 import com.htc.domain.entities.utility.parameters.DateCreated;
 import com.htc.domain.entities.utility.parameters.Id;
+import com.htc.domain.entities.utility.parameters.file.FileName;
 import com.htc.domain.entities.utility.parameters.file.FileUrlPath;
-import lombok.Getter;
 
 /**
  * Контент.
  */
-public class Content {
+public interface Content {
   /**
-   * Идентификатор контента.
+   * Идентификатор.
+   *
+   * @see Id#create(Long)
    *
    * @return id идентификатор
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter Id id;
+  Id getId();
 
   /**
    * {@link Type Тип} контента.
    *
    * @return type тип
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter Type type;
+  Type getType();
 
   /**
    * Наименование контента.
    *
    * @return name наименование
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter String name;
+  FileName getName();
 
   /**
    * Дата создания.
    *
+   * @see DateCreated#create()
+   *
    * @return dateCreated дата создания
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter DateCreated dateCreated;
+  DateCreated getDateCreated();
 
   /**
    * Идентификатор {@link com.htc.domain.entities.user.User автора контента}.
    *
-   *
    * @return id идентификатор автора
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter User author;
-
-  /**
-   * {@link Format Формат} файла.
-   *
-   * @return format формат
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter Format format;
+  User getAuthor();
 
   /**
    * Путь к превью.
    *
+   * @see FileUrlPath#create(String)
+   *
    * @return previewPath путь к превью
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter FileUrlPath previewPath;
+  FileUrlPath getPreviewPath();
 
   /**
    * Идентификатор {@link com.htc.domain.entities.file.File файла}.
    *
    * @return id идентификатор файла
    */
-  @SuppressWarnings("JavadocDeclaration")
-  private @Getter File file;
-
-  /**
-   * Идентификатор {@link com.htc.domain.entities.task.Task задачи}.
-   *
-   * @return id идентификатор задачи
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  //TODO task
-  private @Getter int taskId;
+  File getFile();
 }
