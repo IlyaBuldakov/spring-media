10981d8868d698d07c2436ce4f864d2d0d14074a
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-17 17:09:11 +0400

    feat: Добавлено представление сущности комментария для базы данных.

---
+++ src/main/java/com/htc/infrastructure/models/comment/CommentModel.java
@@ -0,0 +1,68 @@
+package com.htc.infrastructure.models.comment;
+
+import com.htc.domain.entities.comment.Comment;
+import com.htc.domain.entities.user.User;
+import com.htc.domain.entities.utility.parameters.DateCreated;
+import com.htc.domain.entities.utility.parameters.Id;
+import com.htc.infrastructure.models.user.UserModel;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+
+/**
+ * Представление сущности комментария в СУБД.
+ */
+@Entity
+@Table(name = "COMMENTS")
+@AllArgsConstructor
+public class CommentModel implements Comment {
+  @javax.persistence.Id
+  @GeneratedValue
+  @Column(name = "id")
+  Long commentId;
+
+  @Column(name = "dateCreated")
+  String dateCreated;
+
+  @ManyToOne
+  @JoinColumn(name = "authorId")
+  UserModel author;
+
+  @Column(name = "message")
+  String message;
+
+  @Column(name = "contents")
+  //TODO hibernate custom type [] // org.hibernate.mapping?
+  String contents;
+
+  @Override
+  public Id getId() {
+    return Id.create(this.commentId).get();
+  }
+
+  @Override
+  public DateCreated getDateCreated() {
+    return DateCreated.create(this.dateCreated).get();
+  }
+
+  @Override
+  public User getAuthor() {
+    return this.author;
+  }
+
+  @Override
+  public String getMessage() {
+    return this.message;
+  }
+
+  @Override
+  public int[] getContents() {
+    return new int[0];
+  }
+
+  protected CommentModel() {}
+}
