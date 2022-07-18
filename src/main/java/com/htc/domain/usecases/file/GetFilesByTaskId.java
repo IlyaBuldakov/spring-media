3a0e6266a24141e7d972dd2033dbad2ed3dc8cb0
Yaroslav <yaroslav-w@mail.ru>
2022-07-19 01:33:53 +0400

    fix: Переработаны сценарии использования сущности файл.

--- src/main/java/com/htc/domain/usecases/file/GetFilesByTaskId.java
+++ src/main/java/com/htc/domain/usecases/file/GetFilesByTaskId.java
@@ -1,11 +1,15 @@
 package com.htc.domain.usecases.file;
 
+import com.htc.domain.entities.File;
+import com.htc.domain.entities.attributes.Id;
 import com.htc.domain.entities.failures.Failure;
-import com.htc.domain.entities.files.File;
+import com.htc.domain.entities.failures.InvalidValues;
 import com.htc.domain.repositories.FileRepository;
 import com.htc.domain.usecases.UseCase;
+import com.htc.utility.Results;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +18,26 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @AllArgsConstructor
-public class GetFilesByTaskId implements UseCase<Integer, Iterable<File>> {
+public final class GetFilesByTaskId implements UseCase<GetFilesByTaskId.Params, Collection<File>> {
+  /**
+   * Параметры сценария получения файла по его идентификатору.
+   *
+   * @param id Идентификатор задачи.
+   * @param key Ключ идентификатора задачи.
+   */
+  public record Params(int id, String key) {}
+
   private final FileRepository repository;
 
   @Override
-  public Future<Either<Failure, Iterable<File>>> execute(Integer taskId) {
-    return repository.getFilesByTaskId(taskId);
+  public CompletableFuture<Either<Failure, Collection<File>>> execute(Params params) {
+    var taskId = Id.create(params.id);
+    if (taskId.isRight()) {
+      return repository.getFilesByTaskId(taskId.get().getValue());
+    }
+
+    var failure = new InvalidValues();
+    failure.invalidValues().put(taskId.getLeft(), params.key);
+    return Results.fail(failure);
   }
 }
--- src/main/java/com/htc/domain/usecases/file/UploadFile.java
+++ src/main/java/com/htc/domain/usecases/file/UploadFile.java
@@ -1,11 +1,12 @@
 package com.htc.domain.usecases.file;
 
+import com.htc.domain.entities.File;
 import com.htc.domain.entities.failures.Failure;
-import com.htc.domain.entities.files.File;
 import com.htc.domain.repositories.FileRepository;
 import com.htc.domain.usecases.UseCase;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,38 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @AllArgsConstructor
-public class UploadFile implements UseCase<File, File> {
+public final class UploadFile implements UseCase<UploadFile.Params, File> {
+  /**
+   * Параметры сценария загрузки файла.
+   *
+   * @param name Название файла.
+   * @param nameKey Ключ названия файла.
+   * @param dateCreated Дата создания файла.
+   * @param dateCreatedKey Ключ даты создания файла.
+   * @param format Формат файла.
+   * @param formatKey Ключ формата файла.
+   * @param urlFile Путь к файлу.
+   * @param urlFileKey Ключ пути пользователя.
+   * @param taskId Идентификатор задачи содержащей файл.
+   * @param taskIdKey Ключ идентификатора задачи содержащей файл.
+   */
+  public record Params(
+      String name, String nameKey,
+      LocalDateTime dateCreated, String dateCreatedKey,
+      File.Format format, String formatKey,
+      String urlFile, String urlFileKey,
+      int taskId, String taskIdKey) {}
+
   private final FileRepository repository;
 
   @Override
-  public Future<Either<Failure, File>> execute(File file) {
-    return repository.upload(file);
+  public CompletableFuture<Either<Failure, File>> execute(Params params) {
+    return repository.upload(
+        params.name,
+        params.dateCreated,
+        params.format,
+        params.urlFile,
+        params.taskId
+    );
   }
 }
