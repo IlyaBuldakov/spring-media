0fccac0a8c0c9364e45bf5fca5ad95fb5b1a5e29
Yaroslav <yaroslav-w@mail.ru>
2022-07-18 17:00:21 +0400

    fix: Переработаны методы в репозитории контента.

--- src/main/java/com/htc/domain/repositories/ContentRepository.java
+++ src/main/java/com/htc/domain/repositories/ContentRepository.java
@@ -1,9 +1,11 @@
 package com.htc.domain.repositories;
 
-import com.htc.domain.entities.content.Content;
+import com.htc.domain.entities.Content;
 import com.htc.domain.entities.failures.Failure;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Репозиторий контента.
@@ -12,29 +14,38 @@ public interface ContentRepository {
   /**
    * Загружает контент в задачу.
    *
-   * @param content Контент.
+   * @param type Тип контента.
+   * @param name Название контента.
+   * @param dateCreate Дата создания контента.
+   * @param authorId Идентификатор автора контента.
+   * @param format Формат контента.
+   * @param urlContent Путь к контенту.
+   * @param urlPreview Путь к превью контента.
+   * @param taskId Идентификатор задачи содержащей контент.
+   * @return Контент или Ошибка.
    */
-  Future<Either<Failure, Content>> upload(Content content);
+  CompletableFuture<Either<Failure, Content>> upload(
+      Content.Type type,
+      String name,
+      LocalDateTime dateCreate,
+      int authorId,
+      Content.Format format,
+      String urlContent,
+      String urlPreview,
+      int taskId
+  );
 
   /**
    * Удаляет контент.
    *
    * @param id Идентификатор контента.
    */
-  Future<Either<Failure, Void>> delete(int id);
-
-  /**
-   * Получает контент по идентификатору задачи.
-   *
-   * @param taskId Идентификатор задачи.
-   * @return Контент.
-   */
-  Future<Either<Failure, Iterable<Content>>> getContentsByTaskId(int taskId);
+  CompletableFuture<Either<Failure, Void>> delete(int id);
 
   /**
    * Получает содержимое ленты контента.
    *
    * @return Весь контент.
    */
-  Future<Either<Failure, Iterable<Content>>> getContentFeed();
+  CompletableFuture<Either<Failure, Collection<Content>>> getContentFeed();
 }
