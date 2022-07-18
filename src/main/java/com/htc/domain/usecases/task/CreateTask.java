fe1ef929e17cd84f855316b118db520e7f914e85
Yaroslav <yaroslav-w@mail.ru>
2022-07-19 01:19:16 +0400

    feat: Заготовка сценария создания сущности задач.

--- src/main/java/com/htc/domain/usecases/task/CreateTask.java
+++ src/main/java/com/htc/domain/usecases/task/CreateTask.java
@@ -1,11 +1,13 @@
 package com.htc.domain.usecases.task;
 
+import com.htc.domain.entities.Content;
+import com.htc.domain.entities.Task;
 import com.htc.domain.entities.failures.Failure;
-import com.htc.domain.entities.task.Task;
 import com.htc.domain.repositories.TaskRepository;
 import com.htc.domain.usecases.UseCase;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +16,35 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @AllArgsConstructor
-public class CreateTask implements UseCase<Task, Task> {
+public class CreateTask implements UseCase<CreateTask.Params, Task> {
+  public record Params(
+      String name, String nameKey,
+      Content.Type type, String typeKey,
+      String description, String descriptionKey,
+      int[] fileId, String fileIdKey,
+      int authorId, String authorIdKey,
+      int executorId, String executorIdKey,
+      LocalDateTime dateCreated, String dateCreatedKey,
+      LocalDateTime dateExpired, String dateExpiredKey,
+      int[] contentsId, String contentsIdKey,
+      int[] commentsId, String commentsIdKey,
+      Task.Status status, String statusKey) {}
+
   private final TaskRepository repository;
 
   @Override
-  public Future<Either<Failure, Task>> execute(Task task) {
-    return repository.create(task);
+  public CompletableFuture<Either<Failure, Task>> execute(Params params) {
+    return repository.create(
+        params.name(),
+        params.type(),
+        params.description(),
+        params.fileId(),
+        params.authorId(),
+        params.executorId,
+        params.dateCreated,
+        params.dateExpired,
+        params.contentsId,
+        params.commentsId,
+        params.status);
   }
 }
