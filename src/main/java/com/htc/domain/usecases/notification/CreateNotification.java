835b157935e2df76c57e4a7c36e51322e817c75d
Yaroslav <yaroslav-w@mail.ru>
2022-07-19 01:17:56 +0400

    feat: Заготовка сценария создания сущности уведомления.

--- src/main/java/com/htc/domain/usecases/notification/CreateNotification.java
+++ src/main/java/com/htc/domain/usecases/notification/CreateNotification.java
@@ -1,24 +1,33 @@
-//package com.htc.domain.usecases.notification;
-//
-//import com.htc.domain.entities.failures.Failure;
-//import com.htc.domain.entities.notification.Notification;
-//import com.htc.domain.repositories.NotificationRepository;
-//import com.htc.domain.usecases.UseCase;
-//import io.vavr.control.Either;
-//import java.util.concurrent.Future;
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * Сценарий создания задачи.
-// */
-//@Component
-//@AllArgsConstructor
-//public class CreateNotification implements UseCase<Notification, Notification> {
-//  private final NotificationRepository repository;
-//
-//  @Override
-//  public Future<Either<Failure, Notification>> execute(Notification notification) {
-//    return repository.create(notification);
-//  }
-//}
+package com.htc.domain.usecases.notification;
+
+import com.htc.domain.entities.Notification;
+import com.htc.domain.entities.User;
+import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.repositories.NotificationRepository;
+import com.htc.domain.usecases.UseCase;
+import io.vavr.control.Either;
+import java.util.concurrent.CompletableFuture;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Сценарий создания задачи.
+ */
+@Component
+@AllArgsConstructor
+public class CreateNotification implements UseCase<CreateNotification.Params, Notification> {
+
+  public record Params(
+      Notification.NotificationType notificationType,
+      String notificationMessage, String emailKey,
+      String password, String passwordKey,
+      String image, String imageKey,
+      User.Role role, String roleKey) {}
+
+  private final NotificationRepository repository;
+
+  @Override
+  public CompletableFuture<Either<Failure, Notification>> execute(Params params) {
+    return null;
+  }
+}
