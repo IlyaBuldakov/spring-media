3f85b2facb167a7e411ee48fa30382d2d65456e8
Yaroslav <yaroslav-w@mail.ru>
2022-07-18 17:01:16 +0400

    fix: Переработаны методы в репозитории файла.

--- src/main/java/com/htc/domain/repositories/FileRepository.java
+++ src/main/java/com/htc/domain/repositories/FileRepository.java
@@ -1,9 +1,12 @@
 package com.htc.domain.repositories;
 
+import com.htc.domain.entities.File;
+import com.htc.domain.entities.attributes.Id;
 import com.htc.domain.entities.failures.Failure;
-import com.htc.domain.entities.files.File;
 import io.vavr.control.Either;
-import java.util.concurrent.Future;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Репозиторий файлов.
@@ -13,21 +16,32 @@ public interface FileRepository {
   /**
    * Загружает файл в задачу.
    *
-   * @param file Файл.
+   * @param name Название файла.
+   * @param dateCreated Дата загрузки файла.
+   * @param format Формат файла.
+   * @param urlFile Путь к файлу.
+   * @param taskId Идентификатор задачи файла.
+   * @return Файл или ошибку.
    */
-  Future<Either<Failure, File>> upload(File file);
+  CompletableFuture<Either<Failure, File>> upload(
+      String name,
+      LocalDateTime dateCreated,
+      File.Format format,
+      String urlFile,
+      int taskId
+  );
 
   /**
    * Удаляет файл из задачи.
    *
    * @param id Идентификатор файла.
    */
-  Future<Either<Failure, Void>> delete(int id);
+  CompletableFuture<Either<Failure, Void>> delete(Id id);
 
   /**
-   * Получает файл под идентификатору задачи.
+   * Получает файлы под идентификатору задачи.
    *
    * @param taskId Идентификатор задачи.
    */
-  Future<Either<Failure, Iterable<File>>> getFilesByTaskId(int taskId);
+  CompletableFuture<Either<Failure, Collection<File>>> getFilesByTaskId(int taskId);
 }
