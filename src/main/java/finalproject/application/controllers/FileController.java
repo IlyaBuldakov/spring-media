b421435cc12b0d2dfefadd9ae5496ad6173146ef
Ilya Voenkov <voenkov@gmail.com>
2022-07-19 17:19:14 +0400

    feat: в класс контроллера добавлен метод

--- src/main/java/finalproject/application/controllers/FileController.java
+++ src/main/java/finalproject/application/controllers/FileController.java
@@ -1,36 +1,45 @@
 package finalproject.application.controllers;
 
+
 import finalproject.application.services.FileStorageService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.servlet.http.HttpServletRequest;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
-  @AllArgsConstructor
-  @RestController
-  public class FileController {
+@AllArgsConstructor
+@RestController
+public class FileController {
 
-    FileStorageService fileStorageService;
+  FileStorageService fileStorageService;
 
 
-    @ApiOperation(value = "", authorizations = { @Authorization(value="Bearer") })
-    @PostMapping("/api/contents")
-    public String uploadContent(@RequestParam("file") MultipartFile file, HttpServletRequest request){
-      String filename =  StringUtils.cleanPath(file.getOriginalFilename());
-      String type = file.getContentType();
-      Path path = Paths.get("./src/main/resources/static/content");
-      fileStorageService.save(file, path);
-      return filename + " " + type;
 
-    }
+  @ApiOperation(value = "", authorizations = { @Authorization(value="Bearer") })
+  @PostMapping("/api/contents")
+  public String uploadContent(@RequestPart("file") MultipartFile file, @RequestPart("task") int task_id, HttpServletRequest request) {
+    String filename = StringUtils.cleanPath(file.getOriginalFilename());
+    String type = file.getContentType();
+    Path path = Paths.get("./src/main/resources/static/content");
+    fileStorageService.save(file, path);
+    return task_id + " " + type;
+  }
+
+  @ApiOperation(value = "", authorizations = { @Authorization(value="Bearer") })
+  @PostMapping("/api/files")
+  public String uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest request){
+    String filename =  StringUtils.cleanPath(file.getOriginalFilename());
+    String type = file.getContentType();
+    Path path = Paths.get("./src/main/resources/static/files");
+    fileStorageService.save(file, path);
+    return filename + " " + type;
 
   }
 
+}
