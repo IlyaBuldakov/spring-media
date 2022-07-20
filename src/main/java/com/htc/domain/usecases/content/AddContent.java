e2e4a46909a79cf37ef752f950996702422f6ffd
YuriyKrasnoperov <yurakr89@gmail.com>
2022-07-20 23:48:26 +0400

    fix: Доработаны сценарии использования контента.

--- src/main/java/com/htc/domain/usecases/content/AddContent.java
+++ src/main/java/com/htc/domain/usecases/content/AddContent.java
@@ -1,18 +1,15 @@
 package com.htc.domain.usecases.content;
 
 import com.htc.domain.entities.content.Content;
-import com.htc.domain.entities.content.Type;
 import com.htc.domain.entities.failures.Failure;
 import com.htc.domain.entities.failures.InvalidValueParam;
 import com.htc.domain.entities.failures.InvalidValues;
 import com.htc.domain.entities.file.File;
-import com.htc.domain.entities.user.User;
+import com.htc.domain.entities.task.Task;
 import com.htc.domain.entities.utility.parameters.Id;
-import com.htc.domain.entities.utility.parameters.file.FileName;
-import com.htc.domain.entities.utility.parameters.file.FileUrlPath;
 import com.htc.domain.repositories.ContentRepository;
 import com.htc.domain.repositories.FileRepository;
-import com.htc.domain.repositories.UserRepository;
+import com.htc.domain.repositories.TaskRepository;
 import com.htc.domain.usecases.UseCase;
 import com.htc.utility.EitherHelper;
 import io.vavr.control.Either;
@@ -30,53 +27,38 @@ public final class AddContent implements UseCase<AddContent.Params, Content> {
   /**
    * Параметры сценария добавления контента.
    *
-   * @param name наименование контента
-   * @param nameKey ключ наименования контента
-   * @param type тип контента
-   * @param typeKey ключ типа контента
-   * @param authorId идентификатор автора контента
-   * @param authorKey ключ идентификатора автора контента
-   * @param previewPath превью контента
-   * @param previewPathKey ключ превью контента
+   * @param taskId идентификатор задачи, относящийся к контенту
+   * @param taskKey ключ идентификатора задачи, относящейся к контенту
    * @param fileId идентификатор файла, относящийся к контенту
    * @param fileKey ключ идентификатора файла, относящийся к контенту
    */
-  public record Params(String name, String nameKey,
-                       Type type, String typeKey,
-                       Long authorId, String authorKey,
-                       String previewPath, String previewPathKey,
+  public record Params(Long taskId, String taskKey,
                        Long fileId, String fileKey) {}
 
   private final ContentRepository repository;
-  private final UserRepository userRepository;
   private final FileRepository fileRepository;
+  private final TaskRepository taskRepository;
 
   @Override
   public CompletableFuture<Either<Failure, Content>> execute(Params params) {
     var failure = new InvalidValues();
     //TODO возможно filename переименовать в EntityName
-    var name = FileName.create(params.name());
-    if (name.isLeft()) {
-      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_NAME, params.nameKey);
-    }
-    User user = null;
+    Task task = null;
     try {
-      user = userRepository.get(Id.create(params.authorId()).get()).get().get();
+      task = taskRepository.get(Id.create(params.taskId()).get()).get().get();
     } catch (InterruptedException | ExecutionException e) {
-      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, "user not found");
-    }
-    var previewPath = FileUrlPath.create(params.previewPath());
-    if (previewPath.isLeft()) {
-      failure.getValues().put(InvalidValueParam.INVALID_FILE_URL_PATH, params.previewPathKey);
+      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, "task not found");
     }
     File file = null;
     try {
-      file = fileRepository.get(Id.create(params.authorId()).get()).get().get();
+      file = fileRepository.get(Id.create(params.fileId()).get()).get().get();
     } catch (InterruptedException | ExecutionException e) {
       failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, "file not found");
     }
+    //TODO проверять на null??
     return failure.getValues().size() == 0
-            ? repository.add(name.get(), params.type(), user, previewPath.get(), file)
+            ? repository.add(task.getName(), task.getType(), task.getAuthor(),
+            file.getFormat(), file.getFileUrlPath(), file, task)
             : EitherHelper.badLeft(failure);
   }
 }
--- src/main/java/com/htc/domain/usecases/content/ChangeContentById.java
+++ src/main/java/com/htc/domain/usecases/content/ChangeContentById.java
@@ -1,18 +1,15 @@
 package com.htc.domain.usecases.content;
 
 import com.htc.domain.entities.content.Content;
-import com.htc.domain.entities.content.Type;
 import com.htc.domain.entities.failures.Failure;
 import com.htc.domain.entities.failures.InvalidValueParam;
 import com.htc.domain.entities.failures.InvalidValues;
 import com.htc.domain.entities.file.File;
-import com.htc.domain.entities.user.User;
+import com.htc.domain.entities.task.Task;
 import com.htc.domain.entities.utility.parameters.Id;
-import com.htc.domain.entities.utility.parameters.file.FileName;
-import com.htc.domain.entities.utility.parameters.file.FileUrlPath;
 import com.htc.domain.repositories.ContentRepository;
 import com.htc.domain.repositories.FileRepository;
-import com.htc.domain.repositories.UserRepository;
+import com.htc.domain.repositories.TaskRepository;
 import com.htc.domain.usecases.UseCase;
 import com.htc.utility.EitherHelper;
 import io.vavr.control.Either;
@@ -31,27 +28,18 @@ public final class ChangeContentById implements UseCase<ChangeContentById.Params
    *
    * @param id идентификатор контента
    * @param idKey ключ идентификатора контента
-   * @param name наименование контента
-   * @param nameKey ключ наименования контента
-   * @param type тип контента
-   * @param typeKey ключ типа контента
-   * @param authorId автор контента
-   * @param authorKey ключ автора контента
-   * @param previewPath превью контента
-   * @param previewPathKey ключ превью контента
-   * @param fileId файл, относящийся к контенту
-   * @param fileKey ключ файла, относящийся к контенту
+   * @param taskId идентификатор задачи, относящийся к контенту
+   * @param taskKey ключ идентификатора задачи, относящейся к контенту
+   * @param fileId идентификатор файла, относящийся к контенту
+   * @param fileKey ключ идентификатора файла, относящийся к контенту
    */
   public record Params(Long id, String idKey,
-                       String name, String nameKey,
-                       Type type, String typeKey,
-                       Long authorId, String authorKey,
-                       String previewPath, String previewPathKey,
+                       Long taskId, String taskKey,
                        Long fileId, String fileKey) {}
 
   private final ContentRepository repository;
-  private final UserRepository userRepository;
   private final FileRepository fileRepository;
+  private final TaskRepository taskRepository;
 
   @Override
   public CompletableFuture<Either<Failure, Content>> execute(Params params) {
@@ -60,28 +48,22 @@ public final class ChangeContentById implements UseCase<ChangeContentById.Params
     if (id.isLeft()) {
       failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, params.idKey);
     }
-    var name = FileName.create(params.name());
-    if (name.isLeft()) {
-      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_NAME, params.nameKey);
-    }
-    User user = null;
+    Task task = null;
     try {
-      user = userRepository.get(Id.create(params.authorId()).get()).get().get();
+      task = taskRepository.get(Id.create(params.taskId()).get()).get().get();
     } catch (InterruptedException | ExecutionException e) {
-      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, "user not found");
-    }
-    var previewPath = FileUrlPath.create(params.previewPath());
-    if (previewPath.isLeft()) {
-      failure.getValues().put(InvalidValueParam.INVALID_FILE_URL_PATH, params.previewPathKey);
+      failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, "task not found");
     }
     File file = null;
     try {
-      file = fileRepository.get(Id.create(params.authorId()).get()).get().get();
+      file = fileRepository.get(Id.create(params.fileId()).get()).get().get();
     } catch (InterruptedException | ExecutionException e) {
       failure.getValues().put(InvalidValueParam.INVALID_ENTITY_ID, "file not found");
     }
+    //TODO проверять на null??
     return failure.getValues().size() == 0
-            ? repository.change(id.get(), name.get(), params.type(), user, previewPath.get(), file)
+            ? repository.change(id.get(), task.getName(), task.getType(), task.getAuthor(),
+            file.getFormat(), file.getFileUrlPath(), file, task)
             : EitherHelper.badLeft(failure);
   }
 }
