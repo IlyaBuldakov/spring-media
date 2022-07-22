d86fbdfd8b225e7dd730452bdd9409a8bc652edc
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-22 09:44:53 +0400

    feat: Добавлен репозиторий для работы с файлами

---
+++ src/main/java/com/htc/domain/repositories/FilesRepository.java
@@ -0,0 +1,17 @@
+package com.htc.domain.repositories;
+
+import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.entities.file.File.Format;
+import io.vavr.control.Either;
+
+import java.time.LocalDate;
+import java.util.concurrent.CompletableFuture;
+
+public interface FilesRepository {
+
+    void uploadFile(String name, LocalDate dateCreated, Format format, String url, int taskId);
+
+    CompletableFuture<Either<Failure, Void>> deleteFile(int fileId);
+
+    Either<Failure, String> findFileUrlById(int fileId);
+}
