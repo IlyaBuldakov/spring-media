5b1a1b602f8210dc6535511ede3c8f2552855166
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-22 14:14:35 +0400

    feat: Реализован репозиторий для контента

---
+++ src/main/java/com/htc/infrastructure/jpa/ContentsRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.htc.infrastructure.jpa;
+
+import com.htc.domain.entities.content.Content;
+import com.htc.domain.entities.content.ContentType;
+import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.entities.failures.NotFound;
+import com.htc.domain.repositories.ContentsRepository;
+import com.htc.infrastructure.mappers.ContentMapper;
+import com.htc.infrastructure.repositories.ContentsJpaRepository;
+import io.vavr.control.Either;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@AllArgsConstructor
+public class ContentsRepositoryImpl implements ContentsRepository {
+
+    ContentsJpaRepository contentsJpaRepository;
+
+    @Override
+    public CompletableFuture<Either<Failure, List<Content>>> getAll() {
+        return CompletableFuture.completedFuture(Either.right(
+                new ArrayList<>(contentsJpaRepository.findAll())
+        ));
+    }
+
+    @Override
+    public CompletableFuture<Either<Failure, Void>> create(String name, ContentType type, Content.Format format, int taskId) {
+        contentsJpaRepository.save(new ContentMapper(name, type, format, taskId));
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Either<Failure, Void>> deleteById(int id) {
+        try {
+            contentsJpaRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            return CompletableFuture.completedFuture(Either.left(NotFound.CONTENT));
+        }
+        return CompletableFuture.completedFuture(Either.right(null));
+    }
+}
