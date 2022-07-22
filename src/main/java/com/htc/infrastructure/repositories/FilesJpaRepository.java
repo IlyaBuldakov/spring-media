fcf35e5431a270ac05d9e8b16a9904da8b80c23b
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-22 11:00:07 +0400

    feat: Добавлен метод поиска файлов, содержащих заданный task id

--- src/main/java/com/htc/infrastructure/repositories/FilesJpaRepository.java
+++ src/main/java/com/htc/infrastructure/repositories/FilesJpaRepository.java
@@ -3,5 +3,9 @@ package com.htc.infrastructure.repositories;
 import com.htc.infrastructure.mappers.FileMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface FilesJpaRepository extends JpaRepository<FileMapper, Integer> {
+
+    Set<FileMapper> findFileMappersByTaskId(int id);
 }
