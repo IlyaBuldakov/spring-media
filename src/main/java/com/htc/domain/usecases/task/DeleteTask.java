7d4293a2e433ea146b9c3d1823c549dbd5ab48e3
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-22 11:03:19 +0400

    feat: Добавлен метод удаления статических ресурсов при удалении задачи

--- src/main/java/com/htc/domain/usecases/task/DeleteTask.java
+++ src/main/java/com/htc/domain/usecases/task/DeleteTask.java
@@ -1,12 +1,14 @@
 package com.htc.domain.usecases.task;
 
 import com.htc.domain.entities.failures.Failure;
+import com.htc.domain.repositories.FilesRepository;
 import com.htc.domain.repositories.TasksRepository;
 import com.htc.util.ValuesValidator;
 import io.vavr.control.Either;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.util.concurrent.CompletableFuture;
 
 @Component
@@ -15,12 +17,23 @@ public class DeleteTask {
 
     TasksRepository tasksRepository;
 
+    FilesRepository filesRepository;
+
     public CompletableFuture<Either<Failure, Void>> execute(String id) {
         var expectedFailure = ValuesValidator.validateStringId(id);
         if (expectedFailure != null) {
             return CompletableFuture.completedFuture(Either.left(expectedFailure));
         }
+        clearRelevantStaticResources(Integer.parseInt(id));
         tasksRepository.deleteById(Integer.parseInt(id));
         return null;
+    }
+
+    private void clearRelevantStaticResources(int id) {
+        String pathQualifier = "src/main/webapp/";
+        var fileIds = filesRepository.findRelevantToTaskFiles(id);
+        for (String url : fileIds) {
+            new File(pathQualifier + url).delete();
+        }
     }
 }
