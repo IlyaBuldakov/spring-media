313c5b09b87bb40db7d53d4432dc60374b8f01c9
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-22 06:18:26 +0400

    feat: Добавлена конфигурация (ограничения) входящих файлов

--- src/main/java/com/htc/Application.java
+++ src/main/java/com/htc/Application.java
@@ -2,8 +2,13 @@ package com.htc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableAsync
@@ -11,5 +16,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofKilobytes(128));
+        factory.setMaxRequestSize(DataSize.ofKilobytes(128));
+        return factory.createMultipartConfig();
     }
 }
