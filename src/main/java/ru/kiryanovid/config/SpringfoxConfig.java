cf4523ce425c78029e035547d55470913444770e
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-07-24 10:41:13 +0400

    feat: добавлен класс конфигурации springdoc

---
+++ src/main/java/ru/kiryanovid/config/SpringfoxConfig.java
@@ -0,0 +1,14 @@
+package ru.kiryanovid.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SpringfoxConfig {
+}
