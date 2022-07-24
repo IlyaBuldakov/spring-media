65609fcb6dd38b16428b9b45f57e7d6de653d7da
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-07-24 16:48:56 +0400

    fix: Классы DTO уведомлений преобразованы в записи.

--- src/main/java/com/htc/application/dto/notification/NotificationDto.java
+++ src/main/java/com/htc/application/dto/notification/NotificationDto.java
@@ -8,62 +8,31 @@ import lombok.Getter;
 
 /**
  * Представление сущности уведомления.
+ *
+ * @param id Индентификатор уведомления.
+ * @param type Тип уведомления.
+ * @param date Дата получения уведомления.
+ * @param message Сообщение уведомления.
+ * @param user Получатель уведомления.
+ * @param task Задача. Задача связанная с уведомлнием.
  */
-public class NotificationDto {
-  /**
-   * Индентификатор уведомления.
-   *
-   * @return Индентификатор уведомления.
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private final @Getter int id;
-  /**
-   * Тип уведомления.
-   *
-   * @return Тип уведомления.
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private final @Getter Notification.Type type;
-  /**
-   * Дата получения уведомления.
-   *
-   * @return Дата плучения уведомления.
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private final @Getter LocalDateTime date;
-  /**
-   * Сообщение уведомления.
-   *
-   * @return Сообщение уведомления.
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private final @Getter String message;
-  /**
-   * Получатель уведомления.
-   *
-   * @return Пользователь - получатель уведомления.
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private final @Getter UserShortResponse user;
-  /**
-   * Задача. Задача связанная с уведомлнием.
-   *
-   * @return Задача.
-   */
-  @SuppressWarnings("JavadocDeclaration")
-  private final @Getter TaskCardResponse task;
-
-  /**
+public record NotificationDto(int id,
+                              Notification.Type type,
+                              LocalDateTime date,
+                              String message,
+                              UserShortResponse user,
+                              TaskCardResponse task) {
+    /**
    * Создаёт экземпляр класса {@link NotificationDto}.
    *
    * @param notification Уведомление.
    */
   public NotificationDto(Notification notification) {
-    this.id = notification.getId().getValue();
-    this.type = notification.getType();
-    this.date = notification.getDate();
-    this.message = notification.getMessage().getValue();
-    this.user = new UserShortResponse(notification.getUser());
-    this.task = new TaskCardResponse(notification.getTask());
+    this(notification.getId().getValue(),
+      notification.getType(),
+      notification.getDate(),
+      notification.getMessage().getValue(),
+      new UserShortResponse(notification.getUser()),
+      new TaskCardResponse(notification.getTask()));
   }
 }
