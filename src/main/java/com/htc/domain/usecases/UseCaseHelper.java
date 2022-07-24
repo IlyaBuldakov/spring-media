89b5d577e48f5535bb10647ffd4d9f6218ac640b
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-24 21:33:00 +0400

    feat: Добавлен вспомогательный класс для проверки права доступа по роли

--- src/main/java/com/htc/domain/usecases/UseCaseHelper.java
+++ src/main/java/com/htc/domain/usecases/UseCaseHelper.java
@@ -1,14 +1,26 @@
-//package com.htc.domain.usecases;
-//
-//import com.htc.domain.entities.failures.Failure;
-//import com.htc.domain.entities.failures.InvalidValuesContainer;
-//import io.vavr.control.Either;
-//
-//import java.util.concurrent.CompletableFuture;
-//
-//public class UseCaseHelper {
-//
-//    public static <T> CompletableFuture<Either<Failure, T>> validate(InvalidValuesContainer expectedFailure) {
-//
-//    }
-//}
+package com.htc.domain.usecases;
+
+import com.htc.domain.entities.user.Role;
+
+import java.util.Set;
+
+public class UseCaseHelper {
+
+    /**
+     * Логика проверки прав доступа пользователя по роли.
+     *
+     * @param permissions Права доступа.
+     * @param roles Массив ролей.
+     * @return boolean.
+     */
+    public static boolean hasRolePermissions(Set<String> permissions, Role ... roles) {
+        int localRoleCounter = 0;
+        for (String permission : permissions) {
+            Role expectedRole = Role.lookup(permission);
+            if (expectedRole == roles[localRoleCounter++]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
