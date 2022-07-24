1086660dd82e36952077f7a415ff5a80d33e2900
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-24 21:29:19 +0400

    feat: Добавлен вспомогательный класс для преобразования прав доступа

---
+++ src/main/java/com/htc/application/services/ServiceHelper.java
@@ -0,0 +1,23 @@
+package com.htc.application.services;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ServiceHelper {
+
+    /**
+     * Получение представления прав доступа
+     * в виде строк (permissions).
+     *
+     * @return Уникальное множество прав доступа.
+     */
+    public static Set<String> getPermissions(Collection<? extends GrantedAuthority> authorities) {
+        return authorities
+                .parallelStream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+    }
+}
