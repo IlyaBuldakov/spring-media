743666f411b446734550f0707862db097c6c69b2
Aleksandr Aziatcev <a.s.aziattsev@gmail.com>
2022-07-24 09:58:19 +0400

    feat: Создан интефейс для создания миниатюры медиаконтента.

---
+++ src/main/java/com/htc/service/PreviewPicture.java
@@ -0,0 +1,20 @@
+package com.htc.service;
+
+import com.htc.domain.entities.content.Content;
+import java.io.File;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Интефейс для создания миниатюры медиаконтента.
+ */
+public interface PreviewPicture {
+
+  File createPreview(MultipartFile file, Content.Type type);
+
+  File createVideoPreview(MultipartFile file);
+
+  File createPhotoPreview(MultipartFile file);
+
+  File createAudioPreview(MultipartFile file);
+
+}
---
+++ src/main/java/com/htc/service/impl/PreviewPictureImpl.java
@@ -0,0 +1,37 @@
+package com.htc.service.impl;
+
+import com.htc.domain.entities.content.Content;
+import com.htc.service.PreviewPicture;
+import java.io.File;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Реализация интефейса для создания миниатюры медиаконтента.
+ */
+@Service
+public class PreviewPictureImpl implements PreviewPicture {
+  @Override
+  public File createPreview(MultipartFile file, Content.Type type) {
+    return switch (type) {
+      case VIDEO -> createVideoPreview(file);
+      case PHOTO -> createPhotoPreview(file);
+      default -> createAudioPreview(file);
+    };
+  }
+
+  @Override
+  public File createVideoPreview(MultipartFile file) {
+    return null;
+  }
+
+  @Override
+  public File createPhotoPreview(MultipartFile file) {
+    return null;
+  }
+
+  @Override
+  public File createAudioPreview(MultipartFile file) {
+    return null;
+  }
+}
