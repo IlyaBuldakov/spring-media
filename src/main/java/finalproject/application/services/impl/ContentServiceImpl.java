3fed85b1fc396e1896c19c842c8d32d910bbee87
Ilya Voenkov <voenkov@gmail.com>
2022-07-25 17:27:50 +0400

    feat: реализовано превью для фото

--- src/main/java/finalproject/application/services/impl/ContentServiceImpl.java
+++ src/main/java/finalproject/application/services/impl/ContentServiceImpl.java
@@ -5,11 +5,16 @@ package finalproject.application.services.impl;
 import finalproject.application.services.ContentService;
 import finalproject.application.services.FileStorageService;
 import finalproject.domain.entities.content.ContentFormat;
+import finalproject.domain.entities.content.ContentType;
 import finalproject.domain.entities.failures.Failure;
 import finalproject.domain.entities.filedocuments.FileDocuments;
 import finalproject.infrastructure.repositories.ContentRepository;
 import finalproject.infrastructure.repositories.TaskRepository;
 import io.vavr.control.Either;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,6 +29,8 @@ import org.apache.tika.Tika;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
 
 /**
  * Реализация сервиса контента.
@@ -58,14 +65,38 @@ public class ContentServiceImpl implements ContentService {
     }
 
     Path path = Paths.get(contentPath, Integer.toString(taskId));
-    if (fileStorageService.save(file, path, filename)) {
-      return CompletableFuture.completedFuture(Either.right(
-              returnRelativePath + taskId + "/" + filename));
-    } else {
+    if (!fileStorageService.save(file, path, filename)) {
       return CompletableFuture.completedFuture(Either.left(
               new Failure(Failure.Messages.INTERNAL_SERVER_ERROR)));
     }
+    String fileUrl = returnRelativePath + taskId + "/" + filename;
+    String previewUrl = returnRelativePath + taskId + "/" + "preview_" +
+            filename.substring(0, filename.length() - 4) + ".jpg";
+    getPreviewUrl(extension, filename, path);
+    return CompletableFuture.completedFuture(Either.right(previewUrl + "\n" + fileUrl));
   }
+
+  public void getPreviewUrl(String extension, String filename, Path path) throws IOException {
+    ContentType type = switch(extension.toLowerCase()) {
+      case "jpg", "png" -> ContentType.PHOTO;
+      case "mp3", "m4a", "flac" -> ContentType.AUDIO;
+      case "avi", "mp4" -> ContentType.VIDEO;
+      default -> null;
+    };
+
+    if (type == ContentType.PHOTO) {
+      BufferedImage preview = ImageIO.read(new File(path.resolve(filename).toString()));
+      Image resultingImage = preview.getScaledInstance(640, 480, Image.SCALE_DEFAULT);
+      BufferedImage outputImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+      outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+      File output = new File(path.resolve("preview_" + filename.substring(0, filename.length() - 4) + ".jpg").toString());
+      ImageIO.write(outputImage, "jpg", output);
+    }
+
+
+  }
+
+
 
 
 
@@ -82,4 +113,5 @@ public class ContentServiceImpl implements ContentService {
       getAllFilesRelatedToTask(int taskId) {
     return null;
   }
+
 }
