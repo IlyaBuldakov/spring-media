1fdb31a5f75813dbd2de2192bf76821e5f79a01a
Игорь Кирьянов <igor.kiryanov.1986@mail.ru>
2022-07-25 21:53:44 +0400

    fix: переименован класс главного обработчика ошибок

--- src/main/java/ru/kiryanovid/application/controllers/GeneralExceptionHandler.java
+++ src/main/java/ru/kiryanovid/application/controllers/GeneralExceptionHandler.java
@@ -9,7 +9,7 @@ import ru.kiryanovid.application.dto.errors.FailureException;
  * Главный обработчик ошибок.
  */
 @ControllerAdvice
-public class MyExceptionHandler {
+public class GeneralExceptionHandler {
   @ExceptionHandler({FailureException.class})
   public static ResponseEntity<FailureException> handleException(FailureException exception) {
     return new ResponseEntity<>(exception, exception.getStatus());
