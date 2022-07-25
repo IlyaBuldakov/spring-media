31f6d1b5a43a7c053c3f0919baf0aa3872556d1c
Ilya Voenkov <voenkov@gmail.com>
2022-07-25 10:24:39 +0400

    fix: правки по checkstyle

--- src/main/java/finalproject/application/services/ContentService.java
+++ src/main/java/finalproject/application/services/ContentService.java
@@ -3,7 +3,6 @@ package finalproject.application.services;
 import finalproject.domain.entities.failures.Failure;
 import finalproject.domain.entities.file.File;
 import io.vavr.control.Either;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -13,7 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
  * Сервис контента.
  */
 public interface ContentService {
-  CompletableFuture<Either<Failure, String>> attachFileToTask(MultipartFile file, int taskId) throws IOException;
+  CompletableFuture<Either<Failure, String>> attachFileToTask(
+          MultipartFile file, int taskId) throws IOException;
 
   CompletableFuture<Either<Failure, Void>> deleteFileById(int id);
 
--- src/main/java/finalproject/utils/Validators.java
+++ src/main/java/finalproject/utils/Validators.java
@@ -15,7 +15,8 @@ import org.apache.commons.lang3.Validate;
 public class Validators {
 
   static final String EMAIL_PATTERN =
-          "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$";
+          "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@[^-]"
+                  + "[\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$";
 
   static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,20})";
 
