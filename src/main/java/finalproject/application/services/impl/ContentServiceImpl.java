54460fa17880b7435707260726717c634f7befa4
Ilya Voenkov <voenkov@gmail.com>
2022-07-26 16:08:07 +0400

    feat: реализован захват видео для preview

--- src/main/java/finalproject/application/services/impl/ContentServiceImpl.java
+++ src/main/java/finalproject/application/services/impl/ContentServiceImpl.java
@@ -12,7 +12,7 @@ import finalproject.infrastructure.repositories.ContentRepository;
 import finalproject.infrastructure.repositories.TaskRepository;
 import io.vavr.control.Either;
 
-import java.awt.*;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +26,9 @@ import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.tika.Tika;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +42,7 @@ import javax.imageio.ImageIO;
 @AllArgsConstructor
 public class ContentServiceImpl implements ContentService {
 
+  static final String staticPath = "./src/main/resources/static";
   static final String contentPath = "./src/main/resources/static/content";
   static final String returnRelativePath = "/content/";
   FileStorageService fileStorageService;
@@ -56,9 +60,15 @@ public class ContentServiceImpl implements ContentService {
     String extension = FilenameUtils.getExtension(originalFilename);
     Tika tika = new Tika();
     String mimeType = tika.detect(file.getInputStream());
+
     if (!Arrays.stream(ContentFormat.values()).map(Enum::toString)
             .toList().contains(extension.toUpperCase())
-            || !mimeType.contains(extension.toLowerCase())) {
+            ||
+                    !(mimeType.contains("audio")
+                    || mimeType.contains("video")
+                    || mimeType.contains("image"))
+
+    ) {
       problems.add("file");
       return CompletableFuture.completedFuture(Either.left(
               new Failure(Failure.Messages.UNACCEPTABLE_FILE_FORMAT, problems)));
@@ -70,13 +80,13 @@ public class ContentServiceImpl implements ContentService {
               new Failure(Failure.Messages.INTERNAL_SERVER_ERROR)));
     }
     String fileUrl = returnRelativePath + taskId + "/" + filename;
-    String previewUrl = returnRelativePath + taskId + "/" + "preview_" +
-            filename.substring(0, filename.length() - 4) + ".jpg";
-    getPreviewUrl(extension, filename, path);
-    return CompletableFuture.completedFuture(Either.right(previewUrl + "\n" + fileUrl));
+    String previewUrl = getPreviewUrl(extension, filename, path);
+
+    return CompletableFuture.completedFuture(Either.right(fileUrl + " " + previewUrl));
+
   }
 
-  public void getPreviewUrl(String extension, String filename, Path path) throws IOException {
+  public String getPreviewUrl(String extension, String filename, Path path) throws IOException {
     ContentType type = switch(extension.toLowerCase()) {
       case "jpg", "png" -> ContentType.PHOTO;
       case "mp3", "m4a", "flac" -> ContentType.AUDIO;
@@ -89,11 +99,29 @@ public class ContentServiceImpl implements ContentService {
       Image resultingImage = preview.getScaledInstance(640, 480, Image.SCALE_DEFAULT);
       BufferedImage outputImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
       outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
-      File output = new File(path.resolve("preview_" + filename.substring(0, filename.length() - 4) + ".jpg").toString());
+      String previewPath = path.resolve("preview_"
+              + filename.substring(0, filename.lastIndexOf(".")) + ".jpg").toString();
+      File output = new File(previewPath);
       ImageIO.write(outputImage, "jpg", output);
+      return "/" + Paths.get(staticPath).toUri().relativize(Paths.get(previewPath).toUri());
     }
 
-
+    if (type == ContentType.VIDEO) {
+      String previewPath = path.resolve("preview_"
+              + filename.substring(0, filename.lastIndexOf(".")) + ".jpg").toString();
+      File output = new File(previewPath);
+      FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(path.resolve(filename).toString());
+      grabber.start();
+      Frame frame = grabber.grabImage();
+      Java2DFrameConverter converter = new Java2DFrameConverter();
+      BufferedImage bi = converter.convert(frame);
+      Image resultingImage = bi.getScaledInstance(640, 480, Image.SCALE_DEFAULT);
+      BufferedImage outputImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+      outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+      ImageIO.write(outputImage, "jpg", output);
+      return "/" + Paths.get(staticPath).toUri().relativize(Paths.get(previewPath).toUri());
+    }
+return null;
   }
 
 
