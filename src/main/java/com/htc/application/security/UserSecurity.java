d74a62427e4039a3434f35800ee1e8939046ef37
IlyaBuldakov <ggpanguin@gmail.com>
2022-07-26 13:02:18 +0400

    feat: Добавлено представление пользователя для Spring Security

---
+++ src/main/java/com/htc/application/security/UserSecurity.java
@@ -0,0 +1,71 @@
+package com.htc.application.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Реализация контракта пользователя Spring Security.
+ */
+@AllArgsConstructor
+public class UserSecurity implements UserDetails {
+
+    /**
+     * E-mail пользователя.
+     * (в контексте Spring Security - username, см. {@link UserDetailsServiceImpl})
+     */
+    private String email;
+
+    /**
+     * Пароль пользователя.
+     */
+    private String password;
+
+    /**
+     * Права пользователя.
+     */
+    private Set<GrantedAuthority> authorities;
+
+    /**
+     * Статус пользователя.
+     */
+    private boolean isActive;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
