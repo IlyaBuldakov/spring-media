59b31f30a7a1b13ad92f096f5e528cafa644ec99
Ilya Voenkov <voenkov@gmail.com>
2022-07-29 13:13:33 +0400

    feat: добавлено изменение задачи

--- src/main/java/finalproject/application/controllers/TaskController.java
+++ src/main/java/finalproject/application/controllers/TaskController.java
@@ -1,12 +1,16 @@
 package finalproject.application.controllers;
 
 import finalproject.application.dto.failures.BadRequestDto;
+import finalproject.application.dto.failures.InternalServerErrorDto;
 import finalproject.application.dto.failures.NotFoundDto;
 import finalproject.application.dto.task.TaskRequestDto;
 import finalproject.application.services.TaskService;
 import finalproject.application.services.UserService;
 import finalproject.domain.entities.content.ContentType;
+import finalproject.domain.entities.failures.BadRequest;
+import finalproject.domain.entities.failures.NotFound;
 import finalproject.domain.entities.task.Task;
+import finalproject.domain.entities.task.TaskStatus;
 import finalproject.domain.entities.user.User;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
@@ -15,10 +19,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Контроллер задач.
@@ -51,8 +52,36 @@ public class TaskController {
             taskdto.getDescription(), author, contentMaker,
             taskdto.getDateExpired()).getOrElseThrow(failure -> new BadRequestDto(failure));
     task.setDateCreated(LocalDateTime.now());
+    task.setTaskStatus(TaskStatus.INWORK);
     return taskService.createNewTask(task).thenApply(either -> either.get());
 
+  }
+
+  @ApiOperation(value = "", authorizations = { @Authorization(value = "Bearer") })
+  @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER')")
+  @PutMapping("/{id}")
+  public CompletableFuture<Task> editTask(@PathVariable int id, @RequestBody TaskRequestDto taskdto) throws ExecutionException, InterruptedException {
+    User author = userService.getUserById(taskdto.getAuthorId())
+            .thenApply(either -> either.getOrElseThrow(failure -> new NotFoundDto(failure)))
+            .get();
+    User contentMaker = userService.getUserById(taskdto.getContentMakerId())
+            .thenApply(either -> either.getOrElseThrow(failure -> new NotFoundDto(failure)))
+            .get();
+    Task updatedTask = Task.create(taskdto.getName(), ContentType.getContentTypeByName(taskdto.getType()),
+                                  taskdto.getDescription(), author, contentMaker,
+                                  taskdto.getDateExpired())
+                            .getOrElseThrow(failure -> new BadRequestDto(failure));
+    updatedTask.setTaskStatus(TaskStatus.getTaskStatusByName(taskdto.getTaskStatus()));
+    return taskService.editTask(updatedTask, id).thenApply(
+            either -> either.getOrElseThrow(failure -> {
+              if (failure instanceof BadRequest) {
+                return new BadRequestDto(failure);
+              }
+              if (failure instanceof NotFound) {
+                return new NotFoundDto(failure);
+              }
+              return new InternalServerErrorDto(failure);
+            }));
 
   }
 
--- src/main/java/finalproject/application/services/impl/TaskServiceImpl.java
+++ src/main/java/finalproject/application/services/impl/TaskServiceImpl.java
@@ -1,10 +1,15 @@
 package finalproject.application.services.impl;
 
 import finalproject.application.services.TaskService;
-import finalproject.domain.entities.failures.Failure;
+import finalproject.domain.entities.content.Content;
+import finalproject.domain.entities.failures.*;
 import finalproject.domain.entities.task.Task;
+import finalproject.domain.entities.task.TaskStatus;
+import finalproject.infrastructure.repositories.ContentRepository;
 import finalproject.infrastructure.repositories.TaskRepository;
 import io.vavr.control.Either;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
@@ -20,6 +25,7 @@ public class TaskServiceImpl implements TaskService {
 
 
   private final TaskRepository taskRepository;
+  private final ContentRepository contentRepository;
 
 
 
@@ -32,7 +38,21 @@ public class TaskServiceImpl implements TaskService {
 
   @Override
   public CompletableFuture<Either<Failure, Task>> editTask(Task task, int id) {
-    return null;
+    if (!taskRepository.existsById(id)) {
+      return CompletableFuture.completedFuture(
+              Either.left(new NotFound(Messages.TASK_NOT_FOUND)));
+    }
+    Task oldTask = taskRepository.findById(id).get();
+    task.setDateCreated(oldTask.getDateCreated());
+    if (task.getTaskStatus() == TaskStatus.APPROVED && oldTask.getTaskStatus() != TaskStatus.APPROVED) {
+      for (Content content : contentRepository.findByTaskId(task.getId())) {
+        content.setIsPublished(true);
+        contentRepository.save(content);
+      }
+
+    }
+
+    return CompletableFuture.completedFuture(Either.right(taskRepository.save(task)));
   }
 
   @Override
@@ -42,15 +62,21 @@ public class TaskServiceImpl implements TaskService {
 
   @Override
   public CompletableFuture<Either<Failure, Task>> getTaskById(int id) {
+    List<String> problems = new ArrayList<>();
+    if (id < 1) {
+      problems.add("id");
+      return CompletableFuture.completedFuture(Either.left(
+              new BadRequest(Messages.INVALID_VALUES, problems)));
+    }
     if (!taskRepository.existsById(id)) {
       return CompletableFuture.completedFuture(
-              Either.left(new Failure(Failure.Messages.ENTITY_NOT_FOUND)));
+              Either.left(new NotFound(Messages.TASK_NOT_FOUND)));
     }
     if (taskRepository.findById(id).isPresent()) {
       return CompletableFuture.completedFuture(Either.right(taskRepository.findById(id).get()));
     } else {
       return CompletableFuture.completedFuture(
-              Either.left(new Failure(Failure.Messages.INTERNAL_SERVER_ERROR)));
+              Either.left(new InternalServerError(Messages.INTERNAL_SERVER_ERROR)));
     }
   }
 
